30d417427ca4db86662ad3714906db6a7783db8b
Jacob Stoebel <stoebelj@berea.edu>
2015-09-23 16:52:41 -0400

    clinical_teachers

--- app/models/clinical_teacher.rb
+++ app/models/clinical_teacher.rb
@@ -4,16 +4,30 @@ class ClinicalTeacher < ActiveRecord::Base
 	belongs_to :clinical_site
 
   	EMAIL_REGEX = /\A[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}\Z/i
-  	validates :Email, 
-		:presence => true,
-	    :format => EMAIL_REGEX
+  	validates :FirstName,
+  		:presence => {message: "Please enter a first name."}
 
-	validate do |teacher|
-		teacher.errors.add(:base, "Please enter a first name.") if teacher.FirstName.blank?
-		teacher.errors.add(:base, "Please enter a last name.") if teacher.LastName.blank?
-		teacher.errors.add(:base, "Please select a subject.") if teacher.Subject.blank?
-		teacher.errors.add(:base, "Please select a school") if teacher.clinical_site_id.blank?
-		teacher.errors.add(:base, "Please enter a valid rank (1-3).") if not [1..3].include? teacher.Rank		
-		teacher.errors.add(:base, "Please enter years of experience.") if teacher.YearsExp.blank?			
-	end
+  	validates :LastName,
+  		:presence => {message: "Please enter a last name."}
+
+  	validates :Subject,
+  		:presence => {message: "Please enter a subject."}
+
+  	validates :clinical_site_id,
+  		:presence => {message: "Please enter a school."}
+
+  	validates :Rank,
+  		:numericality => {
+			only_integer: true,
+			greater_than: 0,
+			less_than: 4,
+  			message: "Please enter a valid rank (1-3)."}
+
+  	validates :YearsExp,
+  		:presence => {message: "Please enter years of experience (integer)."},
+  		:numericality => {only_integer: true, message: "Please enter years of experience (integer)."}
+
+  	validates :Email, 
+	    :format => {with: EMAIL_REGEX, message: "Please enter a valid email address."}
+
 end
