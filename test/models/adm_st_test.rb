ce5e6032881efc608ba7d395c5f283daab009229
Jacob Stoebel <stoebelj@berea.edu>
2015-10-27 16:56:58 -0400

    fix model names

--- test/models/adm_st_test.rb
+++ test/models/adm_st_test.rb
@@ -5,10 +5,21 @@ class AdmStTest < ActiveSupport::TestCase
   #   assert true
   # end
 
-  test "needs a student" do
-  	app = AdmSt.new {Student_Bnum: }
+  # @@stu = Student.first
+  # @@app = AdmSt.new(Student_Bnum: @@stu.Bnum, BannerTerm_BannerTerm: 201511)
+
+  # def test_forign_keys
+ #  	puts "TESTING!"
+ #  	app = AdmSt.new #@@app.clone
+	# # app.save  	
+ # #  	assert_raise Mysql2::Error do
+ # #  		app.save
+	# # end
+	# assert true
+  # end
 
 
 
-  end
+
+
 end
