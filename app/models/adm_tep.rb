df8af3a9d9eded642aab1d4aeb75a49955600be8
Jacob Stoebel <stoebelj@berea.edu>
2015-10-29 17:12:00 -0400

    add tets to adm_tep

--- app/models/adm_tep.rb
+++ app/models/adm_tep.rb
@@ -7,6 +7,7 @@ class AdmTep < ActiveRecord::Base
 
   belongs_to :program, {foreign_key: "Program_ProgCode"}
   belongs_to :student, {foreign_key: "Student_Bnum"}
+  belongs_to :banner_term, {foreign_key: "BannerTerm_BannerTerm"}
 
   scope :admitted, lambda { where("TEPAdmit = ?", true)}
 
@@ -24,15 +25,16 @@ class AdmTep < ActiveRecord::Base
     term = BannerTerm.find(app.BannerTerm_BannerTerm)
     next_term = BannerTerm.all.order(:BannerTerm).where("BannerTerm >?", app.BannerTerm_BannerTerm).first
     student = Student.find(app.Student_Bnum)
-    app.errors.add(:base, "Please select a program.") if app.Program_ProgCode.blank?
-    app.errors.add(:base, "Please select a student to apply.") if app.Student_Bnum.blank?
-    app.errors.add(:base, "Admission date must be after term begins.") if app.TEPAdmitDate and app.TEPAdmitDate < term.StartDate
-    app.errors.add(:base, "Admission date must be before next term begins.") if app.TEPAdmitDate and app.TEPAdmitDate >= next_term.StartDate
-    app.errors.add(:base, "Admission date must be given.") if app.TEPAdmit and app.TEPAdmitDate.blank?
-
+    # app.errors.add(:base, "Please select a program.") if app.Program_ProgCode.blank?
+    # app.errors.add(:base, "Please select a student to apply.") if app.Student_Bnum.blank?
+    
+    app.errors.add(:TEPAdmitDate, "Admission date must be given.") if app.TEPAdmit and app.TEPAdmitDate.blank?
+    app.errors.add(:TEPAdmitDate, "Admission date must be after term begins.") if app.TEPAdmitDate and app.TEPAdmitDate < term.StartDate
+    app.errors.add(:TEPAdmitDate, "Admission date must be before next term begins.") if app.TEPAdmitDate and app.TEPAdmitDate >= next_term.StartDate
+    
     # app.errors.add(:base, "Student has not passed the Praxis I exam.") if app.TEPAdmit == true and not praxisI_pass(student)
-    app.errors.add(:base, "Student does not have sufficent GPA to be admitted this term.") if app.TEPAdmit and app.GPA < 2.75 and app.GPA_last30 < 3.0
-    app.errors.add(:base, "Student has not earned 30 credit hours.") if app.TEPAdmit and (app.EarnedCredits.nil? or app.EarnedCredits < 30)
+    app.errors.add(:GPA, "Student does not have sufficent GPA to be admitted this term.") if app.TEPAdmit and app.GPA < 2.75 and app.GPA_last30 < 3.0
+    app.errors.add(:GPA_last30, "Student has not earned 30 credit hours.") if app.TEPAdmit and (app.EarnedCredits.nil? or app.EarnedCredits < 30)
     app.errors.add(:base, "Please attach an admission letter.") if (app.letter_file_name == nil and app.TEPAdmit != nil)
     #TODO must have completed EDS150 with C or better to be admitted.
     #TODO must complete 227, 227 or equivilant with a B- or better (what is the equvilant?) 
--- test/models/adm_tep_test.rb
+++ test/models/adm_tep_test.rb
@@ -7,13 +7,34 @@ class AdmTepTest < ActiveSupport::TestCase
 	# 	self.my_assert(app.Student_Bnum, app.student.Bnum.to_s)
  # 	end
 
-   test "needs a program" do
-   	#test validation: needing a program.
-   	 stu = Student.first
-     app = AdmTep.new({Student_Bnum: stu.Bnum})
-     app.valid?
-     
-   end
+  test "needs foreign keys" do
+  	#test validation: needing a program.
+    app = AdmTep.new
+    app.valid?
+    py_assert(app.errors[:Student_Bnum], ["No student selected."])
+    py_assert(app.errors[:Program_ProgCode], ["No program selected."])
+    py_assert(app.errors[:BannerTerm_BannerTerm], ["No term could be determined."])
+  end
+
+  test "admit date empty" do
+
+    # stu = Student.first
+    # prog = Program.first
+    # term = BannerTerm.first
+    app = AdmTep.first
+    app.valid?
+    py_assert(app.errors[:TEPAdmitDate], ["Admission date must be given."])
+
+  end
+
+  test "admit date too early" do
+    app = AdmTep.first
+    app.TEPAdmitDate = Date.strptime("8/31/15", "%m/%d/%Y")
+    app.valid?
+    py_assert(app.errors[:TEPAdmitDate], ["Admission date must be after term begins."])
+
+  end
+
 
 
 end
