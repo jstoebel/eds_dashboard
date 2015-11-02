ff8bf403677a31e237866b2ef2ec3374ec68e52e
Jacob Stoebel <stoebelj@berea.edu>
2015-11-02 17:02:19 -0500

    tests for adm_st

--- test/models/adm_st_test.rb
+++ test/models/adm_st_test.rb
@@ -15,7 +15,38 @@ class AdmStTest < ActiveSupport::TestCase
 		app.letter_file_name = nil
 		app.skip_val_letter = true
 		app.valid?
-		py_assert(app.errors[:base], [])
+		py_assert([], app.errors[:base])
 	end
+
+	test "admitted too early" do
+		app = AdmSt.first
+		app.STAdmitDate = Date.strptime("8/31/2015", "%m/%d/%Y")
+		app.valid?
+		py_assert(["Admission date must be after term begins."], app.errors[:STAdmitDate])
+	end
+
+	test "admitted too late" do
+		app = AdmSt.first
+		app.STAdmitDate = Date.strptime("01/01/2016", "%m/%d/%Y")
+		app.valid?
+		py_assert(["Admission date may not be before next term begins."], app.errors[:STAdmitDate])
+	end
+
+	test "admission date missing" do
+		app = AdmSt.first
+		app.STAdmitDate = nil
+		app.valid?
+		py_assert(["Admission date must be given."], app.errors[:STAdmitDate])
+	end
+
+	test "no date error if not admitted" do
+		app = AdmSt.first
+		app.STAdmitted = false
+		app.valid?
+		py_assert()
+	end
+
+
+
 
 end
