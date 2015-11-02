9552a75a0e308fa3f3ae5be8f310e4051bbf1e60
Jacob Stoebel <stoebelj@berea.edu>
2015-11-02 13:00:19 -0500

    tests for adm_st

--- test/models/adm_st_test.rb
+++ test/models/adm_st_test.rb
@@ -2,4 +2,20 @@ require 'test_helper'
 
 class AdmStTest < ActiveSupport::TestCase
 
+	test "valid letter" do
+		app = AdmSt.first
+		app.letter_file_name = nil
+		app.skip_val_letter = false
+		app.valid?
+		py_assert(app.errors[:base], ["Please attach an admission letter."])
+	end
+
+	test "skip letter validation" do
+		app = AdmSt.first
+		app.letter_file_name = nil
+		app.skip_val_letter = true
+		app.valid?
+		py_assert(app.errors[:base], [])
+	end
+
 end
