5dd63d7635d43c2f9290585def420f0d8dc4b841
Jacob Stoebel <stoebelj@berea.edu>
2015-11-03 13:17:17 -0500

    tests for clinical site

--- app/models/clinical_site.rb
+++ app/models/clinical_site.rb
@@ -1,11 +1,23 @@
 class ClinicalSite < ActiveRecord::Base
 	has_many :clinical_teachers
 
-	validate do |site|
-		site.errors.add(:base, "Please enter a site name.") if site.SiteName.blank?
-		site.errors.add(:base, "Please enter a city.") if site.City.blank?
-		site.errors.add(:base, "Please enter a county.") if site.County.blank?
-		site.errors.add(:base, "Please enter a sistrict.") if site.District.blank?
-	end
+	validates :SiteName,
+		presence: { message: "Please enter a site name."}
+
+	validates :City,
+		presence: {message: "Please enter a city."}
+
+	validates :County,
+		presence: {message: "Please enter a county."}
+
+	validates :District,
+		presence: {message: "Please enter a district."}
+
+	# validate do |site|
+	# 	site.errors.add(:base, "Please enter a site name.") if site.SiteName.blank?
+	# 	site.errors.add(:base, "Please enter a city.") if site.City.blank?
+	# 	site.errors.add(:base, "Please enter a county.") if site.County.blank?
+	# 	site.errors.add(:base, "Please enter a sistrict.") if site.District.blank?
+	# end
 
 end
