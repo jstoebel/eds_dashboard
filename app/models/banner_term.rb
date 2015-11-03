ece389535ece9f6459c32ce5f05e68191d1b83d0
Jacob Stoebel <stoebelj@berea.edu>
2015-11-03 09:43:44 -0500

    remove scope

--- app/models/banner_term.rb
+++ app/models/banner_term.rb
@@ -2,5 +2,4 @@ class BannerTerm < ActiveRecord::Base
 	has_many :adm_tep, foreign_key: "BannerTerm_BannerTerm"
 	has_many :adm_st, foreign_key: "BannerTerm_BannerTerm"
 
-	scope :from_date, ->(date) {where("StartDate <= ? and EndDate >= ?", date, date).first}
 end
