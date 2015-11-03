788e83d9c617cca5b71234c39dc66e5addf4eccf
Jacob Stoebel <stoebelj@berea.edu>
2015-11-03 13:17:17 -0500

    tests for clinical site

--- test/models/clinical_site_test.rb
+++ test/models/clinical_site_test.rb
@@ -1,7 +1,32 @@
 require 'test_helper'
 
 class ClinicalSiteTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+	test "needs site name" do
+		site = ClinicalSite.first
+		site.SiteName = nil
+		site.valid?
+		py_assert(["Please enter a site name."], site.errors[:SiteName])
+	end
+
+	test "needs city" do
+		site = ClinicalSite.first
+		site.City = nil
+		site.valid?
+		py_assert(["Please enter a city."], site.errors[:City])
+	end
+
+	test "needs county" do
+		site = ClinicalSite.first
+		site.County = nil
+		site.valid?
+		py_assert(["Please enter a county."], site.errors[:County])
+	end
+
+	test "needs district" do
+		site = ClinicalSite.first
+		site.District = nil
+		site.valid?
+		py_assert(["Please enter a district."], site.errors[:District])
+	end
 end
