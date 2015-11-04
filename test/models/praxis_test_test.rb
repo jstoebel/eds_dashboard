010bb0a1b25b2aa5dcd49ec2073ee058c5816201
Jacob Stoebel <stoebelj@berea.edu>
2015-11-04 12:41:28 -0500

    model test for praxis_test

--- test/models/praxis_test_test.rb
+++ test/models/praxis_test_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class PraxisTestTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+	test "scope current" do
+		expected = PraxisTest.current.to_a
+		actual = PraxisTest.where("CurrentTest=1").to_a
+		py_assert(expected, actual)
+	end
+
+	
 end
