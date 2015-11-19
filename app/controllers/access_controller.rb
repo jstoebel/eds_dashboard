808e346ec9f19923f8ac0711437f8b76ebae1b4e
Jacob Stoebel <stoebelj@berea.edu>
2015-11-19 16:16:48 -0500

    ldap testing

--- app/controllers/access_controller.rb
+++ app/controllers/access_controller.rb
@@ -1,10 +1,14 @@
 class AccessController < ApplicationController
-  
+  require 'base64'
+
 	layout 'application'
 
   def index
   	@current_term = current_term({exact: false, plan_b: :forward})
-  	
+  	user_pass = Base64.decode64(request.authorization.split(' ')[1])
+	@username = user_pass.split(':')[0]
+
+
   end
 
   def login
