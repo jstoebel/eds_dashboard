babe161c0969bd5d7376cbb4cf574de9eb07b335
Jacob Stoebel <stoebelj@berea.edu>
2015-12-15 15:31:53 -0500

    auth change to students_controller

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -6,11 +6,7 @@ class StudentsController < ApplicationController
   def index
 
     user = current_user
-  	@students = Student.all.current.by_last   #also need to filter for students who are activley enrolled.
-   #  if user.is?"advisor"
-   #    @students = @students.with_prof(user.tep_advisor.AdvisorBnum)   #filter down to authorized advisors.
-   #  end
-  	# @students = Student.all.current.by_last.with_prof(user_bnum)    #also need to filter for students who are activley enrolled.
+  	@students = Student.all.current.by_last.select {|r| can? :read, r }    #also need to filter for students who are activley enrolled.
   end
 
   def show
