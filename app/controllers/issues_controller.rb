bba7675ed16b79fd4ccdf673f1f2009b12ba5d9e
Jacob Stoebel <stoebelj@berea.edu>
2015-12-15 11:23:56 -0500

    finding @issues in controller

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -34,6 +34,7 @@ class IssuesController < ApplicationController
   def index
     @student = find_student(params[:student_id])
     authorize! :show, @student
+    @issues = @student.issues.sorted.select {|r| can? :read, r }
     name_details(@student)    
   end
 
