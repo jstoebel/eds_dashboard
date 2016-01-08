7ec7dd5d0fcc04bc72b46c23874af097209cddf9
Jacob Stoebel <stoebelj@berea.edu>
2016-01-08 07:38:26 -0500

    started clinical_sites

--- test/controllers/clinical_sites_controller_test.rb
+++ test/controllers/clinical_sites_controller_test.rb
@@ -1,34 +1,39 @@
 require 'test_helper'
 
 class ClinicalSitesControllerTest < ActionController::TestCase
+  #all roles have access to this resource
   test "should get index" do
-    get :index
-    assert_response :success
+    role_names.each do |r|
+      load_session(r)
+      get :index
+      assert_response :success
+      py_assert assigns(:sites), ClinicalAssignment.all.select {|a| can? :read, a }
+    end
   end
 
-  test "should get show" do
-    get :show
-    assert_response :success
-  end
+  # test "should get show" do
+  #   get :show
+  #   assert_response :success
+  # end
 
-  test "should get edit" do
-    get :edit
-    assert_response :success
-  end
+  # test "should get edit" do
+  #   get :edit
+  #   assert_response :success
+  # end
 
-  test "should get update" do
-    get :update
-    assert_response :success
-  end
+  # test "should get update" do
+  #   get :update
+  #   assert_response :success
+  # end
 
-  test "should get new" do
-    get :new
-    assert_response :success
-  end
+  # test "should get new" do
+  #   get :new
+  #   assert_response :success
+  # end
 
-  test "should get create" do
-    get :create
-    assert_response :success
-  end
+  # test "should get create" do
+  #   get :create
+  #   assert_response :success
+  # end
 
 end
