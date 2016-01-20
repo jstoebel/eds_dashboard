79d57ee7cd1e1b1f32ba8aa4c671145cf0db1251
Jacob Stoebel <stoebelj@berea.edu>
2016-01-20 16:40:53 -0500

    starting issues_controller_test

--- test/controllers/issues_controller_test.rb
+++ test/controllers/issues_controller_test.rb
@@ -1,19 +1,30 @@
 require 'test_helper'
 
 class IssuesControllerTest < ActionController::TestCase
+  allowed_roles = ["admin", "advisor"]
+
+
+
   test "should get new" do
-    get :new
-    assert_response :success
+    allowed_roles.each do |r|
+      load_session(r)
+      issue = Issue.new
+      student = Student.first
+      get :new, :student_id => student.id
+      assert_response :success
+      py_assert assigns(:issue), issue
+      py_assert assigns(:student), student
+    end
   end
 
-  test "should get create" do
-    get :create
-    assert_response :success
-  end
+  # test "should get create" do
+  #   get :create
+  #   assert_response :success
+  # end
 
-  test "should get show" do
-    get :show
-    assert_response :success
-  end
+  # test "should get show" do
+  #   get :show
+  #   assert_response :success
+  # end
 
 end
