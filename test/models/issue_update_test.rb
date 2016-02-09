62e2d69e590ce922a4ae7953c637c9c61f896576
Jacob Stoebel <stoebelj@berea.edu>
2016-02-09 09:30:29 -0500

    removed regex tests for issue_updates model

--- test/models/issue_update_test.rb
+++ test/models/issue_update_test.rb
@@ -23,41 +23,6 @@ class IssueUpdateTest < ActiveSupport::TestCase
 		py_assert(["Please provide an update description."], t.errors[:Description])
 	end
 
-test "advisor bnum matches regex1" do
-		t = IssueUpdate.first
-		t.tep_advisors_AdvisorBnum = "00123456"
-		t.valid?
-		py_assert(["Please enter a valid B#, (including the B00)"], t.errors[:tep_advisors_AdvisorBnum])
-	end
-
-	test "advisor bnum matches regex2" do
-		t = IssueUpdate.first
-		t.tep_advisors_AdvisorBnum = "B001234567"
-		t.valid?
-		py_assert(["Please enter a valid B#, (including the B00)"], t.errors[:tep_advisors_AdvisorBnum])
-	end
-
-	test "advisor bnum matches regex3" do
-		t = IssueUpdate.first
-		t.tep_advisors_AdvisorBnum = "123456"
-		t.valid?
-		py_assert(["Please enter a valid B#, (including the B00)"], t.errors[:tep_advisors_AdvisorBnum])
-	end
-
-	test "advisor bnum matches regex4" do
-		t = IssueUpdate.first
-		t.tep_advisors_AdvisorBnum = "B0012345"
-		t.valid?
-		py_assert(["Please enter a valid B#, (including the B00)"], t.errors[:tep_advisors_AdvisorBnum])
-	end
-
-	test "advisor bnum matches regex5" do
-		t = IssueUpdate.first
-		t.tep_advisors_AdvisorBnum = "completly off!"
-		t.valid?
-		py_assert(["Please enter a valid B#, (including the B00)"], t.errors[:tep_advisors_AdvisorBnum])
-	end
-
 	test "advisor blank bnum bad" do
 		t = IssueUpdate.first
 		t.tep_advisors_AdvisorBnum = nil
