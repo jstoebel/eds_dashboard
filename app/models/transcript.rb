0e6fea4f2975c6e64b426ff82630c4eaf42ac8c5
Jacob Stoebel <stoebelj@berea.edu>
2016-02-10 09:56:38 -0500

    syntax fix for self.primary_keys

--- app/models/transcript.rb
+++ app/models/transcript.rb
@@ -1,6 +1,6 @@
 class Transcript < ActiveRecord::Base
 	self.table_name = 'transcript'
-    self.primary_keys :crn, :Student_Bnum
+    self.primary_keys = :crn, :Student_Bnum
 	belongs_to :student, {:foreign_key => 'Student_Bnum'}
 
 	scope :in_term, ->(term_object) { where(term_taken: term_object.BannerTerm)}
