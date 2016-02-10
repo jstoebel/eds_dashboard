fcfa7f80c753090fb911a759fb14d2db06f84407
Jacob Stoebel <stoebelj@berea.edu>
2016-02-10 09:25:46 -0500

    changes for advisor_assignment added composite pks and fixed column name to match what model expects

--- app/models/advisor_assignment.rb
+++ app/models/advisor_assignment.rb
@@ -1,5 +1,5 @@
 class AdvisorAssignment < ActiveRecord::Base
-
+	self.primary_keys = :student, :tep_advisor
 	belongs_to :student, {:foreign_key => 'Student_Bnum'}
 	belongs_to :tep_advisor, {:foreign_key => 'tep_advisors_AdvisorBnum'}
 end
