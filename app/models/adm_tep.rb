67ec21f59e61211aea6d398a7c917cd8b92df093
Jacob Stoebel <stoebelj@berea.edu>
2016-02-15 16:49:17 -0500

    fixed .open method in AdmTep

--- app/models/adm_tep.rb
+++ app/models/adm_tep.rb
@@ -15,7 +15,7 @@ class AdmTep < ActiveRecord::Base
   #SCOPES 
   scope :admitted, lambda { where("TEPAdmit = ?", true)}
   #all of a student's open programs.
-  scope :open, ->(bnum) {joins("LEFT JOIN prog_exits ON (adm_tep.Program_ProgCode = prog_exits.Program_ProgCode) and (adm_tep.Student_Bnum = prog_exits.Student_Bnum)").where("prog_exits.id IS NULL AND adm_tep.TEPAdmit = 1 AND adm_tep.Student_Bnum = ?", bnum)}
+  scope :open, ->(bnum) {joins("LEFT JOIN prog_exits ON (adm_tep.Program_ProgCode = prog_exits.Program_ProgCode) and (adm_tep.Student_Bnum = prog_exits.Student_Bnum)").where("prog_exits.ExitCode_ExitCode IS NULL AND adm_tep.TEPAdmit = 1 AND adm_tep.Student_Bnum = ?", bnum)}
   
   scope :by_term, ->(term) {where("BannerTerm_BannerTerm = ?", term)}   #all applications by term
   
