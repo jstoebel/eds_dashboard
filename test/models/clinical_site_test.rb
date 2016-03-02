5c5c712f4a1b661775a94c5e3990ed4e2a49facd
Jacob Stoebel <stoebelj@berea.edu>
2016-03-02 15:37:04 -0500

    updated tests and fixtures for clinical_site new tests and data for new attribs

--- test/models/clinical_site_test.rb
+++ test/models/clinical_site_test.rb
@@ -6,27 +6,90 @@ class ClinicalSiteTest < ActiveSupport::TestCase
 		site = ClinicalSite.first
 		site.SiteName = nil
 		site.valid?
-		py_assert(["Please enter a site name."], site.errors[:SiteName])
+		assert_equal(["Please enter a site name."], site.errors[:SiteName])
 	end
 
 	test "needs city" do
 		site = ClinicalSite.first
 		site.City = nil
 		site.valid?
-		py_assert(["Please enter a city."], site.errors[:City])
+		assert_equal(["Please enter a city."], site.errors[:City])
 	end
 
 	test "needs county" do
 		site = ClinicalSite.first
 		site.County = nil
 		site.valid?
-		py_assert(["Please enter a county."], site.errors[:County])
+		assert_equal(["Please enter a county."], site.errors[:County])
 	end
 
 	test "needs district" do
 		site = ClinicalSite.first
 		site.District = nil
 		site.valid?
-		py_assert(["Please enter a district."], site.errors[:District])
+		assert_equal(["Please enter a district."], site.errors[:District])
 	end
+
+	test "needs phone" do
+		site = ClinicalSite.first
+		site.phone = nil
+		site.valid?
+		assert_equal ["Please enter a phone number"], site.errors[:phone]
+	end
+
+	test "phone number invalid" do
+		site = ClinicalSite.first
+		site.phone = "123"
+		site.valid?
+		assert_equal ["is an invalid number"], site.errors[:phone]
+	end
+
+	test "needs email" do
+		site = ClinicalSite.first
+		site.email = nil
+		site.valid?
+		assert_equal ["Please enter an email.", "Please enter a valid email."], site.errors[:email]
+	end
+
+	test "invalid email no username" do
+		site = ClinicalSite.first
+		site.email = "@berea.edu"
+		site.valid?
+		assert_equal ["Please enter a valid email."], site.errors[:email]
+	end
+
+	test "invalid email no domain" do
+		site = ClinicalSite.first
+		site.email = "feej"
+		site.valid?
+		assert_equal ["Please enter a valid email."], site.errors[:email]
+	end
+
+	test "invalid email no suffix" do
+		site = ClinicalSite.first
+		site.email = "feej@berea"
+		site.valid?
+		assert_equal ["Please enter a valid email."], site.errors[:email]
+	end
+
+	test "needs receptionist" do
+		site = ClinicalSite.first
+		site.receptionist = nil
+		site.valid?
+		assert_equal ["Please enter a receptionist."], site.errors[:receptionist]
+	end 
+
+	test "needs website" do
+		site = ClinicalSite.first
+		site.website = nil
+		site.valid?
+		assert_equal ["Please enter a school website.", "Please enter a valid website." ], site.errors[:website]
+	end
+
+	test "invalid website" do
+		site = ClinicalSite.first
+		site.website = "berea"
+		site.valid?
+		assert_equal ["Please enter a valid website." ], site.errors[:website]
+	end	
 end
