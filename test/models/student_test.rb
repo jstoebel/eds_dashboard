3234d8550c99b2e5bf0ddb8921ae357c2bd9b625
Jacob Stoebel <stoebelj@berea.edu>
2016-03-07 13:13:03 -0500

    added test for praxisI_pass method

--- test/models/student_test.rb
+++ test/models/student_test.rb
@@ -6,28 +6,28 @@ class StudentTest < ActiveSupport::TestCase
 		
 		expected = Student.all.order(LastName: :asc)
 		actual = Student.by_last
-		py_assert(expected.slice(0, expected.size), actual.slice(0, actual.size))
+		assert_equal(expected.slice(0, expected.size), actual.slice(0, actual.size))
 	end
 
 	test "current scope" do
 		#tests the scope called current
 		expected = Student.all.where("ProgStatus in (?) and EnrollmentStatus='Active Student'", ['Candidate', 'Prospective'])
 		actual = Student.current
-		py_assert(expected.slice(0, expected.size), actual.slice(0, actual.size))
+		assert_equal(expected.slice(0, expected.size), actual.slice(0, actual.size))
 
 	end
 
 	test "candidates scope" do
 		expected = Student.all.where("ProgStatus='Candidate' and EnrollmentStatus='Active Student'")
 		actual = Student.candidates
-		py_assert(expected.slice(0, expected.size), actual.slice(0, actual.size))
+		assert_equal(expected.slice(0, expected.size), actual.slice(0, actual.size))
 	end
 
 	test "from alt_id scope" do
 		#pulls a student based on their alt_id.
 
 		s = Student.from_alt_id(2)
-		py_assert(s.ProgStatus, "Candidate")		#this student should be the only one with ProgStatus of "Candidate"
+		assert_equal(s.ProgStatus, "Candidate")		#this student should be the only one with ProgStatus of "Candidate"
 	end
 
 	test "is advisee of passes" do
@@ -82,5 +82,22 @@ class StudentTest < ActiveSupport::TestCase
 		stu = course.student
 		prof_bnum = course.Inst_bnum
 		assert stu.is_student_of("bogus bnum") == false
+	end
+
+	test "praxisI_pass" do
+		Student.all.each do |stu|
+		   	req_tests = PraxisTest.where(TestFamily: 1, CurrentTest: 1).map{ |t| t.TestCode}
+		   	all_passed = PraxisResult.where(Bnum: stu.Bnum, Pass: 1).map{ |p| p.TestCode}
+		   	passing = true
+
+		   	req_tests.each do |requirement|
+		   		if not all_passed.include? requirement
+		   			passing = false	
+	   			end
+		   	end
+	   	
+			assert_equal stu.praxisI_pass, passing
+		end
+
 	end
 end
