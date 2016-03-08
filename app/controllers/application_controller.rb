7af5a412982a8659df99e8fa68248c4dec59a51a
Jacob Stoebel <stoebelj@berea.edu>
2016-03-08 10:58:37 -0500

    generalized term_menu_setup now pulls all terms in the last 2 years as well as current term

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -104,18 +104,14 @@ class ApplicationController < ActionController::Base
 
     if params[:banner_term_id]
       @term = BannerTerm.find(params[:banner_term_id])   #ex: 201412
-
     else
       @term = @current_term   #if no params passed, the term to render is current term
     end
 
-    #assemble possible terms for select menu: all terms more than 
-    #2 years ago, no future terms, and only terms with at least one 
-    #application
+    #assemble possible terms for select menu: all terms less than more than 
+    #2 years ago, no future terms.
 
-    @menu_terms = BannerTerm.joins(:adm_st).group(:BannerTerm).where("StartDate > ? and StartDate < ?", Date.today-730, Date.today)
-    # puts @menu_terms
-    # puts @current_term
+    @menu_terms = BannerTerm.where("StartDate > ? and StartDate < ?", Date.today-730, Date.today)
 
     if (@current_term) and not (@menu_terms.include? @current_term)
       @menu_terms << @current_term    #add the current term if its not there already.
