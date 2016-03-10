87465ea6cac531574bdc584d0f63a090410d458b
Jacob Stoebel <stoebelj@berea.edu>
2016-03-10 12:34:19 -0500

    added attach_letter added a common method to attach a doc to student_files and associate it with the passed in record.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -43,4 +43,16 @@ class ActiveSupport::TestCase
     session[:role] = role.RoleName
   end
 
+  def attach_letter(app)
+    #creates a file and associates it with the application
+    letter = StudentFile.create({
+        :Student_Bnum => app.student.id,
+        :active => true,
+        :doc => fixture_file_upload('test_file.txt')
+      })
+
+    app.student_file_id = letter.id
+    return letter
+  end
+
 end
