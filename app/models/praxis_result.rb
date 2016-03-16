a8a74b9f2e3c2aa768201416ee433b79d69b49b9
Jacob Stoebel <stoebelj@berea.edu>
2016-03-16 16:52:26 -0400

    rollback to old version of praxis_subtest_results uses a single fk, referencing single pk in praxis_results updated models started adding fixtures

--- app/models/praxis_result.rb
+++ app/models/praxis_result.rb
@@ -2,7 +2,7 @@ class PraxisResult < ActiveRecord::Base
 
 	self.primary_keys = :Bnum, :TestCode, :TestDate
 	belongs_to :student, {:foreign_key => "Bnum"}
-	has_many :praxis_subtest_results, {:foreign_key => [:praxis_results_Student_Bnum, :praxis_results_TestCode, :praxis_results_TestDate]}
+	has_many :praxis_subtest_results
 	belongs_to :praxis_test, {:foreign_key => 'TestCode'}
 
 	validates :Bnum,
