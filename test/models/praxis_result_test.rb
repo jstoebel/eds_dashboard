af88d75e51d025f96dce1859ca7863d09e918da4
Jacob Stoebel <stoebelj@berea.edu>
2016-03-17 17:35:57 -0400

    refactoring througout codebase

--- test/models/praxis_result_test.rb
+++ test/models/praxis_result_test.rb
@@ -5,28 +5,28 @@ class PraxisResultTest < ActiveSupport::TestCase
 	
 	test "blank bnum" do
 		t = PraxisResult.first
-		t.Bnum = nil
+		t.student_id = nil
 		t.valid?
 		py_assert(["Please select a student."], t.errors[:Bnum])		
 	end
 
 	test "blank test code" do
 		t = PraxisResult.first
-		t.TestCode = nil
+		t.test_code = nil
 		t.valid?
 		py_assert(["Test must be selected."], t.errors[:TestCode])		
 	end
 
 	test "blank test date" do
 		t = PraxisResult.first
-		t.TestDate = nil
+		t.test_date = nil
 		t.valid?
 		py_assert(["Test date must be selected."], t.errors[:TestDate])		
 	end
 
 	test "blank reg date" do
 		t = PraxisResult.first
-		t.RegDate = nil
+		t.paid_by = nil
 		t.valid?
 		py_assert(["Registration date must be selected."], t.errors[:RegDate])		
 	end
@@ -34,14 +34,14 @@ class PraxisResultTest < ActiveSupport::TestCase
 
 	test "blank paid by" do
 		t = PraxisResult.first
-		t.PaidBy = nil
+		t.paid_by = nil
 		t.valid?
 		py_assert(["Payment source must be given."], t.errors[:PaidBy])		
 	end
 
 	test "bad paid by" do
 		t = PraxisResult.first
-		t.PaidBy = "it was free!"
+		t.paid_by = "it was free!"
 		t.valid?
 		py_assert(["Invalid payment source."], t.errors[:PaidBy])		
 	end
