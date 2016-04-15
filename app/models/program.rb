1249f74fef443634e92a24840f15dac5097b52e6
Jacob Stoebel <stoebelj@berea.edu>
2016-04-15 15:24:03 -0400

    changes to models new: foi and major updated: program

--- app/models/program.rb
+++ app/models/program.rb
@@ -3,4 +3,5 @@ class Program < ActiveRecord::Base
 	has_many :students, {:foreign_key => 'Program_ProgCode', through: :adm_tep}
 	has_many :prog_exits, {:foreign_key => 'Program_ProgCode'}
     has_many :foi
+    has_many :majors
 end
