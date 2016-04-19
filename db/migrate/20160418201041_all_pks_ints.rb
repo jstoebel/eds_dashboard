9812588bf71334072800a2aa25a31692f1b58d00
Jacob Stoebel <stoebelj@berea.edu>
2016-04-19 17:00:48 -0400

    end of day 4/19/16

--- db/migrate/20160418201041_all_pks_ints.rb
+++ db/migrate/20160418201041_all_pks_ints.rb
@@ -1,8 +1,9 @@
 class AllPksInts < ActiveRecord::Migration
   def up
 
-    # students (make Bnum a unique )
-
+    #===========
+    #STUDENTS (make Bnum a unique )
+    #===========
     execute %q(ALTER TABLE `students` 
     DROP INDEX `AltID_UNIQUE`,
     DROP COLUMN `AltID`,
@@ -11,108 +12,167 @@ class AllPksInts < ActiveRecord::Migration
     )
     add_column :students, :id, :primary_key, :first => true
 
-    # tep_advisor (id with Bnum as unique index)
-    execute %q(ALTER TABLE `tep_advisors` 
-    DROP PRIMARY KEY;
-    )
-    add_column :tep_advisors, :id, :primary_key, :first => true
+    #TODO
 
-    # advisor assignments
-    drop_table :advisor_assignments
-
-    create_table :advisor_assignments do |t|
-        t.integer :student_id
-        t.integer :tep_advisor_id
-    end
-
-
-    # EXIT CODES
-
-    #drop the forign key on prog_exits
-
-    remove_foreign_key :prog_exits, :name => "fk_Exit_ExitCode"
-
-    #alter the table
-    execute %q(ALTER TABLE `exit_codes` 
-    ADD COLUMN `id` INT NOT NULL AUTO_INCREMENT FIRST,
-    DROP PRIMARY KEY,
-    ADD PRIMARY KEY (`id`);
-    )
-
-    #change prog_exits to have an int column
-    change_column :prog_exits, :ExitCode_ExitCode, :integer
-
-    #add the forin key to reference the new pk
-    add_foreign_key :prog_exits, :exit_codes, :column => "ExitCode_ExitCode"
-
-    # PRAXIS_RESULTS (set unique index across student/praxis_test and test_date, you'll need to use execute)
+    #for each of these tables:
+        #drop the fk pointing to students
+        #create a new fk to point to new students id
     
-    #remove fks referencing praxis_results
+    #adm_st
+    remove_foreign_key :adm_st, :name => "fk_AdmST_Student"
+    change_column :adm_st, :Student_Bnum, :integer
+    add_foreign_key :adm_st, :students, :column => "Student_Bnum"
 
-    # #remove AltID and make id AI int
-    execute %q(ALTER TABLE `praxis_results` 
-    DROP INDEX `AltID_UNIQUE`,
-    DROP COLUMN `AltID`,
-    CHANGE COLUMN `id` `id` INT NOT NULL AUTO_INCREMENT
-    ;)
+    #adm_tep
+    remove_foreign_key :adm_tep, :name => "fk_AdmTEP_Student"
+    change_column :adm_tep, :Student_Bnum, :integer
+    add_foreign_key :adm_tep, :students, :column => "Student_Bnum" 
 
-    # #add a fk to sub_tests
+    # advisor_assignments -> covered below
 
-    change_column :praxis_subtest_results, :praxis_result_id, :integer
-    add_foreign_key :praxis_subtest_results, :praxis_results
+    # clinical_assignments
+    remove_foreign_key :clinical_assignments, :name => "fk_ClinicalAssignments_Student"
+    change_column :clinical_assignments, :Student_Bnum, :integer
+    add_foreign_key :clinical_assignments, :students, :column => "Student_Bnum"
 
-    #make unique index across three fks
-    add_index :praxis_results, [:student_id, :praxis_test_id, :test_date], {unique: true, :name => "index_by_stu_test_date"}
+    # employment
+    remove_foreign_key :employment, :name => "fk_Employment_Student"
+    change_column :employment, :Student_Bnum, :integer
+    add_foreign_key :employment, :students, :column => "Student_Bnum"
 
-    # PRAXIS_TEST (TestCode should be unique but not pk)
-
-    #remove fk dependancies
-    remove_foreign_key :praxis_results, :name => "fk_praxis_results_praxis_tests"
-    change_column :praxis_results, :praxis_test_id, :integer
-
-
-    remove_foreign_key :praxis_prep, :name => "fk_PraxisPrep_PraxisTest"
-    change_column :praxis_prep, :PraxisTest_TestCode, :integer
+    # forms_of_intention
+    remove_foreign_key :forms_of_intention, :name => "forms_of_intention_student_id_fk"
+    change_column :forms_of_intention, :student_id, :string
+    add_foreign_key :forms_of_intention, :students
     
-    #remove pk
-    execute %q(ALTER TABLE `praxis_tests` 
-    DROP PRIMARY KEY ,
-    ADD UNIQUE INDEX `TestCode_UNIQUE` (`TestCode` ASC);)
-
-    #add id as pk
-    add_column :praxis_tests, :id, :primary_key, :first => true
-
-    #add fks
-    add_foreign_key :praxis_prep, :praxis_tests, :column => :PraxisTest_TestCode
-    add_foreign_key :praxis_results, :praxis_tests
-
-    # praxis_updates (add id)
-    # prog_exit single id with bnum/program as unique index
-    # programs need an id ProgCode is unique
+    # issues
+    # praxis_prep
+    # praxis_result
+    # prog_exits
+    # student_files
     # transcript
+
+
+  #   #===========
+  #   # TEP_ADVISOR (id with Bnum as unique index)
+  #   #===========
+
+  #   execute %q(ALTER TABLE `tep_advisors` 
+  #   DROP PRIMARY KEY;
+  #   )
+  #   add_column :tep_advisors, :id, :primary_key, :first => true
+
+  #   #===========
+  #   # ADVISOR ASSIGNMENTS
+  #   #===========
+
+  #   drop_table :advisor_assignments
+
+  #   create_table :advisor_assignments do |t|
+  #       t.integer :student_id
+  #       t.integer :tep_advisor_id
+    # end
+
+    # add_foreign_key :advisor_assignments, :students, :column => :Student_Bnum
+    # add_foreign_key :advisor_assignments, :tep_advisors, :column => :tep_advisors_AdvisorBnum
+
+  #   #===========
+  #   # EXIT CODES
+  #   #===========
+
+  #   #drop the forign key on prog_exits
+
+  #   remove_foreign_key :prog_exits, :name => "fk_Exit_ExitCode"
+
+  #   #alter the table
+  #   execute %q(ALTER TABLE `exit_codes` 
+  #   ADD COLUMN `id` INT NOT NULL AUTO_INCREMENT FIRST,
+  #   DROP PRIMARY KEY,
+  #   ADD PRIMARY KEY (`id`);
+  #   )
+
+  #   #change prog_exits to have an int column
+  #   change_column :prog_exits, :ExitCode_ExitCode, :integer
+
+  #   #add the forin key to reference the new pk
+  #   add_foreign_key :prog_exits, :exit_codes, :column => "ExitCode_ExitCode"
+
+  #   #===========
+  #   # PRAXIS_RESULTS (set unique index across student/praxis_test and test_date, you'll need to use execute)
+  #   #===========
+  #   #remove fks referencing praxis_results
+
+  #   # #remove AltID and make id AI int
+  #   execute %q(ALTER TABLE `praxis_results` 
+  #   DROP INDEX `AltID_UNIQUE`,
+  #   DROP COLUMN `AltID`,
+  #   CHANGE COLUMN `id` `id` INT NOT NULL AUTO_INCREMENT
+  #   ;)
+
+  #   # #add a fk to sub_tests
+
+  #   change_column :praxis_subtest_results, :praxis_result_id, :integer
+  #   add_foreign_key :praxis_subtest_results, :praxis_results
+
+  #   #make unique index across three fks
+  #   add_index :praxis_results, [:student_id, :praxis_test_id, :test_date],
+  #       {unique: true, :name => "index_by_stu_test_date"}
+
+  #   #===========
+  #   # PRAXIS_TEST (TestCode should be unique but not pk)
+  #   #===========
+  #   #remove fk dependancies
+  #   remove_foreign_key :praxis_results, :name => "fk_praxis_results_praxis_tests"
+  #   change_column :praxis_results, :praxis_test_id, :integer
+
+
+  #   remove_foreign_key :praxis_prep, :name => "fk_PraxisPrep_PraxisTest"
+  #   change_column :praxis_prep, :PraxisTest_TestCode, :integer
+    
+  #   #remove pk
+  #   execute %q(ALTER TABLE `praxis_tests` 
+  #   DROP PRIMARY KEY ,
+  #   ADD UNIQUE INDEX `TestCode_UNIQUE` (`TestCode` ASC);)
+
+  #   #add id as pk
+  #   add_column :praxis_tests, :id, :primary_key, :first => true
+
+  #   #add fks to point to new pk
+  #   add_foreign_key :praxis_prep, :praxis_tests, :column => :PraxisTest_TestCode
+  #   add_foreign_key :praxis_results, :praxis_tests
+
+  #   # PRAXIS_UPDATES (add id)
+  #   # PROG_EXIT single id with bnum/program as unique index
+  #   # PROGRAMS need an id ProgCode is unique
+  #   # TRANSCRIPT
 
   end
 
   def down
 
+    #===========
     # PRAXIS_TEST
+    #===========
 
-    # remove_foreign_key :praxis_results, :praxis_tests
-    # remove_foreign_key :praxis_prep, :column => :PraxisTest_TestCode
+    remove_foreign_key :praxis_results, :praxis_tests
+    remove_foreign_key :praxis_prep, :column => :PraxisTest_TestCode
 
-    # execute %q(ALTER TABLE `eds_development`.`praxis_tests` 
-    # DROP PRIMARY KEY,
-    # DROP COLUMN `id`,
-    # ADD PRIMARY KEY (`TestCode`);
-    # )
+    execute %q(ALTER TABLE `eds_development`.`praxis_tests` 
+    DROP PRIMARY KEY,
+    DROP COLUMN `id`,
+    ADD PRIMARY KEY (`TestCode`);
+    )
 
-    # change_column :praxis_prep, :PraxisTest_TestCode, :string
-    # add_foreign_key "praxis_prep", "praxis_tests", name: "fk_PraxisPrep_PraxisTest", column: "PraxisTest_TestCode", primary_key: "TestCode"
+    change_column :praxis_prep, :PraxisTest_TestCode, :string
+    add_foreign_key "praxis_prep", "praxis_tests", name: "fk_PraxisPrep_PraxisTest", column: "PraxisTest_TestCode", primary_key: "TestCode"
 
-    # change_column :praxis_results, :praxis_test_id, :string
+    change_column :praxis_results, :praxis_test_id, :string
     add_foreign_key "praxis_results", "praxis_tests", name: "fk_PraxisResult_PraxisTest", primary_key: "TestCode"
 
+    #===========
     # PRAXIS_RESULTS
+    #===========
+
     remove_index :praxis_results, :name => "index_by_stu_test_date"
     remove_foreign_key :praxis_subtest_results, :praxis_results
 
@@ -124,7 +184,10 @@ class AllPksInts < ActiveRecord::Migration
     ADD UNIQUE INDEX `AltID_UNIQUE` (`AltID` ASC);
     )
 
+    #===========
     # EXIT CODES
+    #===========
+
     remove_foreign_key :prog_exits, :column => "ExitCode_ExitCode"
     change_column :prog_exits, :ExitCode_ExitCode, :string
     execute %q(ALTER TABLE `exit_codes` 
@@ -134,8 +197,13 @@ class AllPksInts < ActiveRecord::Migration
     )
     add_foreign_key "prog_exits", "exit_codes", name: "fk_Exit_ExitCode", column: "ExitCode_ExitCode", primary_key: "ExitCode"
 
+    #===========
     # ADVISOR ASSIGNMENTS
-    
+    #===========
+
+    remove_foreign_key :advisor_assignments, :column => "Student_Bnum"
+    remove_foreign_key :advisor_assignments, :column => "tep_advisors_AdvisorBnum"
+
     drop_table :advisor_assignments
 
     create_table "advisor_assignments", id: false, force: true do |t|
@@ -143,15 +211,39 @@ class AllPksInts < ActiveRecord::Migration
       t.string "tep_advisors_AdvisorBnum", limit: 9, null: false
     end
 
+    #===========
     #TEP_ADVISORS
+    #===========
     execute %q(ALTER TABLE `eds_development`.`tep_advisors` 
     DROP COLUMN `id`,
     DROP PRIMARY KEY,
     ADD PRIMARY KEY (`AdvisorBnum`);
     )
 
-
+    #===========
     #STUDENTS
+    #===========
+
+    remove_foreign_key :forms_of_intention, :column => "student_id"
+    change_column :forms_of_intention, :student_id, :string
+    add_foreign_key :forms_of_intention, :students
+
+    remove_foreign_key :employment, :column => "Student_Bnum"
+    change_column :employment, :Student_Bnum, :string
+    add_foreign_key "employment", "students", name: "fk_Employment_Student", column: "Student_Bnum", primary_key: "Bnum"
+
+    remove_foreign_key :clinical_assignments, :column => "Student_Bnum"
+    change_column :clinical_assignments, :Student_Bnum, :string
+    add_foreign_key "clinical_assignments", "students", name: "fk_ClinicalAssignments_Student", column: "Bnum", primary_key: "Bnum"
+
+    remove_foreign_key :adm_tep, :column => "Student_Bnum"
+    change_column :adm_tep, :Student_Bnum, :string
+    add_foreign_key "adm_tep", "students", name: "fk_AdmTEP_Student", column: "Student_Bnum", primary_key: "Bnum"
+
+    remove_foreign_key :adm_st, :column => "Student_Bnum"
+    change_column :adm_st, :students, :string
+    add_foreign_key "adm_st", "students", name: "fk_AdmST_Student", column: "Student_Bnum", primary_key: "Bnum"
+
     remove_column :students, :id
 
     execute %q(ALTER TABLE `students` 
