ce4adecb70e113f07a37e7bf1bd8e281775db836
Jacob Stoebel <stoebelj@berea.edu>
2016-04-21 10:44:06 -0400

    migration to refactor praxis_results

--- db/migrate/20160421134937_refactor_praxis_results_id.rb
+++ db/migrate/20160421134937_refactor_praxis_results_id.rb
@@ -1,8 +1,12 @@
 class RefactorPraxisResultsId < ActiveRecord::Migration
   def up
 
-    change_column :praxis_results, :id, :integer
+    change_table :praxis_results do |t|
+        t.remove :id
+        t.remove :AltID
+    end
 
+    add_column :praxis_results, :id, :primary_key, :first => true
     #also add a fk for sub_tests
     change_column :praxis_subtest_results, :praxis_result_id, :integer
     add_foreign_key :praxis_subtest_results, :praxis_results
@@ -12,6 +16,12 @@ class RefactorPraxisResultsId < ActiveRecord::Migration
   def down
     remove_foreign_key :praxis_subtest_results, :praxis_results
     change_column :praxis_subtest_results, :praxis_result_id, :string
-    change_column :praxis_results, :id, :string
+
+    remove_column :praxis_results, :id
+
+    execute %q(ALTER TABLE `praxis_results` 
+    ADD COLUMN `id` VARCHAR(45) NULL FIRST,
+    ADD COLUMN `AltID` INT NULL AUTO_INCREMENT AFTER `pass`,
+    ADD UNIQUE INDEX `AltID_UNIQUE` (`AltID` ASC); )
   end
 end
