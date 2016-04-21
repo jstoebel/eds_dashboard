24b3f3d49a78d9a7c8d74e12ea9cd738a00ed15c
Jacob Stoebel <stoebelj@berea.edu>
2016-04-21 16:25:08 -0400

    random schema refactors

---
+++ db/migrate/20160421194532_pk_refactor_random.rb
@@ -0,0 +1,21 @@
+class PkRefactorRandom < ActiveRecord::Migration
+  def up
+
+    #MAKE banner_terms pk AI
+    execute %q(ALTER TABLE `eds_development`.`banner_terms` 
+    CHANGE COLUMN `BannerTerm` `BannerTerm` INT(11) NOT NULL ;)
+
+
+    #FK clinical_assignment -> banner_term
+    #new lines above this line ^
+
+  end
+
+  def down
+
+    #new lines below this line
+
+    execute %q(ALTER TABLE `eds_development`.`banner_terms` 
+    CHANGE COLUMN `BannerTerm` `BannerTerm` INT(11) NOT NULL AUTO_INCREMENT ;)
+  end
+end
