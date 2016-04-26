2ab770100dbcce27e56ae4131ed89935c8253145
Jacob Stoebel <stoebelj@berea.edu>
2016-04-26 14:17:14 -0400

    removing unused Student method

--- app/models/student.rb
+++ app/models/student.rb
@@ -32,15 +32,6 @@ class Student < ActiveRecord::Base
 		return my_advisors.include?(advisor_profile)
 	end
 
-	# def is_student_of(advisor_profile)
-	# 	#does this student have this prof in the current term (plan_b = forward)
-
-	# 	term = current_term({:exact => false, :plan_b => :forward})
-	# 	classes = self.transcripts.in_term(term)
-	# 	my_profs = classes.map { |i| i.Inst_bnum }
-	# 	return my_profs.include?(advisor_profile.AdvisorBnum)
-	# end
-
 	def is_student_of?(inst_bnum)
 		#does this student have this prof in the current term (plan_b = forward)
 
