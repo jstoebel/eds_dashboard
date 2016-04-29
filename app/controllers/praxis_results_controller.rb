8879cbe343be1a3473cdf466ab968dbd1fe46015
Jacob Stoebel <stoebelj@berea.edu>
2016-04-29 12:45:05 -0400

    helpers and controllers for assessments

--- app/controllers/praxis_results_controller.rb
+++ app/controllers/praxis_results_controller.rb
@@ -1,3 +1,25 @@
+# == Schema Information
+#
+# Table name: praxis_results
+#
+#  id             :integer          not null, primary key
+#  student_id     :integer          not null
+#  praxis_test_id :integer
+#  test_date      :datetime
+#  reg_date       :datetime
+#  paid_by        :string(255)
+#  test_score     :integer
+#  best_score     :integer
+#  cut_score      :integer
+#  pass           :boolean
+#
+# Indexes
+#
+#  fk_praxis_results_praxis_tests_idx  (praxis_test_id)
+#  fk_praxis_results_students_idx      (student_id)
+#  index_by_stu_test_date              (student_id,praxis_test_id,test_date) UNIQUE
+#
+
 class PraxisResultsController < ApplicationController
   layout 'application'
   authorize_resource
--- app/helpers/clinical_assignments_helper.rb
+++ app/helpers/clinical_assignments_helper.rb
@@ -1,2 +1,21 @@
+# == Schema Information
+#
+# Table name: clinical_assignments
+#
+#  student_id          :integer          not null
+#  id                  :integer          not null, primary key
+#  clinical_teacher_id :integer          not null
+#  Term                :integer          not null
+#  CourseID            :string(45)       not null
+#  Level               :string(45)
+#  StartDate           :date
+#  EndDate             :date
+#
+# Indexes
+#
+#  clinical_assignments_student_id_fk           (student_id)
+#  fk_ClinicalAssignments_ClinicalTeacher1_idx  (clinical_teacher_id)
+#
+
 module ClinicalAssignmentsHelper
 end
