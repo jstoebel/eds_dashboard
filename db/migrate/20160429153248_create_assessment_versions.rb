e2920a47c22a0acac9f277290129e1cc1b4bb4dc
Jacob Stoebel <stoebelj@berea.edu>
2016-04-29 12:41:58 -0400

    new migrations for assessments

---
+++ db/migrate/20160429153248_create_assessment_versions.rb
@@ -0,0 +1,15 @@
+class CreateAssessmentVersions < ActiveRecord::Migration
+  def up
+    create_table :assessment_versions do |t|
+      t.integer :assessment_id, :null => false
+      t.integer :version_num
+      t.timestamps
+    end
+
+    add_foreign_key :assessment_versions, :assessments
+  end
+
+  def down
+    drop_table :assessment_versions
+  end
+end
