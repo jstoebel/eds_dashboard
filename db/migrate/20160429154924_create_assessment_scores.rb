b1660e6c5817b80e8d3072fa32021a4314a6ecc3
Jacob Stoebel <stoebelj@berea.edu>
2016-04-29 16:31:25 -0400

    more migrations for asessments create assessment_scores item_levels have a level name (like proficient) and an ord (like 3)

--- db/migrate/20160429154924_create_assessment_scores.rb
+++ db/migrate/20160429154924_create_assessment_scores.rb
@@ -2,13 +2,13 @@ class CreateAssessmentScores < ActiveRecord::Migration
   def up
     create_table :assessment_scores do |t|
       t.integer :student_assessment_id, :null => false
-      t.integer :assessment_item_id, :null => false
+      t.integer :assessment_item_version_id, :null => false
       t.integer :score
       t.timestamps
     end
 
     add_foreign_key :assessment_scores, :student_assessments
-    add_foreign_key :assessment_scores, :assessment_items
+    add_foreign_key :assessment_scores, :assessment_item_versions
 
   end
 
