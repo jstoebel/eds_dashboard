1ef20cf344ce6378721aba50f30e70655fd31904
Jacob Stoebel <stoebelj@berea.edu>
2016-04-29 12:45:05 -0400

    helpers and controllers for assessments

--- app/helpers/clinical_sites_helper.rb
+++ app/helpers/clinical_sites_helper.rb
@@ -1,2 +1,18 @@
+# == Schema Information
+#
+# Table name: clinical_sites
+#
+#  id           :integer          not null, primary key
+#  SiteName     :string(45)       not null
+#  City         :string(45)
+#  County       :string(45)
+#  Principal    :string(45)
+#  District     :string(45)
+#  phone        :string(255)
+#  receptionist :string(255)
+#  website      :string(255)
+#  email        :string(255)
+#
+
 module ClinicalSitesHelper
 end
