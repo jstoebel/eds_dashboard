dd57d988dbc950dadc912830a14292e37a184a73
Jacob Stoebel <stoebelj@berea.edu>
2016-05-02 17:33:21 -0400

    update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,7 @@
 require 'active_record/fixtures'
 
 #load these records from fixtures
-constants = ["banner_terms", "exit_codes", "programs", "roles", "majors"]
+constants = ["banner_terms", "exit_codes", "programs", "roles", "majors", "praxis_tests"]
 constants.each do |c|
     ActiveRecord::FixtureSet::create_fixtures(Rails.root.join("test", "fixtures"), c)
 end
