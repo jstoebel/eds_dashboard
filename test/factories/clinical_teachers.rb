d2907ff3a82f544f52949606d139813a20c924cf
Jacob Stoebel <stoebelj@berea.edu>
2016-05-05 12:05:38 -0400

    factories for clinical_site, teacher and assignment

---
+++ test/factories/clinical_teachers.rb
@@ -0,0 +1,20 @@
+# == Schema Information
+#
+# Table name: clinical_teachers
+#
+#  id               :integer          not null, primary key
+#  Bnum             :string(45)
+#  FirstName        :string(45)       not null
+#  LastName         :string(45)       not null
+#  Email            :string(45)
+#  Subject          :string(45)
+#  clinical_site_id :integer          not null
+#  Rank             :integer
+#  YearsExp         :integer
+#
+
+FactoryGirl.define do
+  factory :clinical_teacher do
+    
+  end
+end
