d33cb834da0e0acb9d6234d87c2f7c0b0c62a417
Jacob Stoebel <stoebelj@berea.edu>
2016-05-06 17:11:56 -0400

    changed models for issue and issue_updates removed garbage validations set association with tep_advisor

--- app/models/issue_update.rb
+++ app/models/issue_update.rb
@@ -13,13 +13,13 @@
 
 class IssueUpdate < ActiveRecord::Base
 	belongs_to :issue, foreign_key: 'Issues_IssueID'
+	belongs_to :tep_advisor, {:foreign_key => 'tep_advisors_AdvisorBnum'}
+
+
 	scope :sorted, lambda {order(:created_at => :desc)}
 
     BNUM_REGEX = /\AB00\d{6}\Z/i
 	validates :UpdateName, 
-		:length => { 
-			maximum: 100,
-			message: "Max name length is 100 characters."},
 		presence: {message: "Please provide an update name."}
 
 	validates :Description,
