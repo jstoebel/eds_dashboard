9e715daf90aa142df8a60e429b27c43fc82b1b59
Jacob Stoebel <stoebelj@berea.edu>
2016-05-11 11:55:21 -0400

    modified set_quality_points only runs if needed attrs are present?

--- app/models/transcript.rb
+++ app/models/transcript.rb
@@ -13,9 +13,9 @@
 #  quality_points    :float(24)
 #  credits_attempted :float(24)
 #  credits_earned    :float(24)
-#  gpa_credits       :float(24)
 #  reg_status        :string(45)
 #  Inst_bnum         :string(45)
+#  gpa_include       :boolean          not null
 #
 
 class Transcript < ActiveRecord::Base
@@ -35,12 +35,16 @@ class Transcript < ActiveRecord::Base
 	scope :in_term, ->(term_object) { where(term_taken: term_object.BannerTerm)}
 
 
+    #~~~VALIDATIONS~~~# 
+
     def set_quality_points
-        #sets quality_points for a record
-        if self.grade_pt_changed? || self.credits_earned_changed?
-            self.quality_points = self.grade_pt / self.credits_earned
+
+        if self.grade_pt.present? && self.credits_earned.present? 
+            #sets quality_points for a record
+            if self.grade_pt_changed? || self.credits_earned_changed?
+                self.quality_points = self.grade_pt * self.credits_earned
+            end
         end
-
-
     end
+    
 end
