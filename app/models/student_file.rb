3ad21bbd87867e7e96d7b83864afbc0d8cc4ba7b
Jacob Stoebel <stoebelj@berea.edu>
2016-05-25 16:42:33 -0400

    simplified validation to ensure uniquness of file name within student

--- app/models/student_file.rb
+++ app/models/student_file.rb
@@ -14,6 +14,7 @@
 class StudentFile < ActiveRecord::Base
 
 	belongs_to :student
+    before_validation :check_file_unique
 
 	has_attached_file :doc, 
 	  :url => "/student_file/:id/download",		#passes AltID 
@@ -23,9 +24,17 @@ class StudentFile < ActiveRecord::Base
 	  
   	validates :doc, attachment_presence: true
   	validates_attachment_file_name :doc, :matches => [/doc\Z/, /docx\Z/, /pdf\Z/, /txt\Z/],
-    	:message => "Attached file must be a Word Document, PDF or plain text document.",
-    	:uniqueness => {:scope => :student_id, :message => "A file with this name already exists for this student."}
+    	:message => "Attached file must be a Word Document, PDF or plain text document."
+
+    validate :doc_file_name, :uniqueness => {scope: :student_id, message: "Document with this name already exists for this student"}
 
 	scope :active, lambda {where(:active => true) }
+
+    private 
+
+    def check_file_unique
+        same_files = StudentFile.where(:student_id => self.student.id, :doc_file_name => self.doc_file_name).where.not(id: self.id)
+        self.errors.add(:base, "Document with this name already exists for this student") if same_files.size > 0 
+    end
 
 end
