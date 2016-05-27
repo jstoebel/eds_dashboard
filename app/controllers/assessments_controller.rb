ff37c0db1e05905d7c1a566f28bed1d5e1eb39ba
Jacob Stoebel <stoebelj@berea.edu>
2016-05-27 16:25:14 -0400

    starting on controller for assessments

---
+++ app/controllers/assessments_controller.rb
@@ -0,0 +1,26 @@
+class AssessmentsController < ApplicationController
+  def index
+    @assessments = Assessment.all
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def delete
+  end
+
+  def destroy
+  end
+end
