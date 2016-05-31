32c0795caddadfcc98543ead0d837b8a5db17aa8
Jacob Stoebel <stoebelj@berea.edu>
2016-05-31 17:04:07 -0400

    annotations for assessments

--- app/controllers/assessments_controller.rb
+++ app/controllers/assessments_controller.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: assessments
+#
+#  id          :integer          not null, primary key
+#  name        :string(255)
+#  description :text
+#  created_at  :datetime
+#  updated_at  :datetime
+#
+
 class AssessmentsController < ApplicationController
   def index
     @assessments = Assessment.all
--- test/controllers/assessments_controller_test.rb
+++ test/controllers/assessments_controller_test.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: assessments
+#
+#  id          :integer          not null, primary key
+#  name        :string(255)
+#  description :text
+#  created_at  :datetime
+#  updated_at  :datetime
+#
+
 require 'test_helper'
 
 class AssessmentsControllerTest < ActionController::TestCase
