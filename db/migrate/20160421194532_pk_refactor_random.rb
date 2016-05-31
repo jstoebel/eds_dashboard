1686bac02cb1319ef8dff591e96124d50073b365
Jacob Stoebel <stoebelj@berea.edu>
2016-05-31 17:03:01 -0400

    new migration for student_scores

--- db/migrate/20160421194532_pk_refactor_random.rb
+++ db/migrate/20160421194532_pk_refactor_random.rb
@@ -2,7 +2,7 @@ class PkRefactorRandom < ActiveRecord::Migration
   def up
 
     #MAKE banner_terms pk AI
-    execute %q(ALTER TABLE `eds_development`.`banner_terms` 
+    execute %q(ALTER TABLE `banner_terms` 
     CHANGE COLUMN `BannerTerm` `BannerTerm` INT(11) NOT NULL ;)
 
 
@@ -15,7 +15,7 @@ class PkRefactorRandom < ActiveRecord::Migration
 
     #new lines below this line
 
-    execute %q(ALTER TABLE `eds_development`.`banner_terms` 
+    execute %q(ALTER TABLE `banner_terms` 
     CHANGE COLUMN `BannerTerm` `BannerTerm` INT(11) NOT NULL AUTO_INCREMENT ;)
   end
 end
