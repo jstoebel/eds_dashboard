dc2fea19db6c655ab152ad2c306bc2faf22ee1ab
Jacob Stoebel <stoebelj@berea.edu>
2016-06-03 16:28:08 -0400

    new annotations

--- app/models/last_name.rb
+++ app/models/last_name.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: last_names
+#
+#  id         :integer          not null, primary key
+#  student_id :integer
+#  last_name  :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 class LastName < ActiveRecord::Base
 	belongs_to :student
 	validates_presence_of :student_id
--- test/models/last_name_test.rb
+++ test/models/last_name_test.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: last_names
+#
+#  id         :integer          not null, primary key
+#  student_id :integer
+#  last_name  :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 require 'test_helper'
 
 class LastNameTest < ActiveSupport::TestCase
