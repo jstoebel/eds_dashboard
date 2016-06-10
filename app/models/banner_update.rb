f9245f43b39da585dfdd42318f82075bcc0848f2
Jacob Stoebel <stoebelj@berea.edu>
2016-06-10 14:01:11 -0400

    added functionality and tests for banner_update

--- app/models/banner_update.rb
+++ app/models/banner_update.rb
@@ -2,21 +2,24 @@
 #
 # Table name: banner_updates
 #
-#  id          :integer          not null, primary key
-#  upload_date :datetime
-#  created_at  :datetime
-#  updated_at  :datetime
+#  id         :integer          not null, primary key
+#  created_at :datetime
+#  updated_at :datetime
+#  start_term :integer
+#  end_term   :integer
 #
 
 class BannerUpdate < ActiveRecord::Base
 
-	before_validation :check_terms
+	# before_validation :check_terms
+    validates_presence_of :start_term, :end_term
+    validates_numericality_of :start_term, :less_than_or_equal_to => :end_term
 
-	private
-	def check_terms
-		if self.start_term >= self.end_term
-			self.errors.add(:base, "Start term must be before end term")
-		end
-	end
+    # private
+	# def check_terms
+	# 	if self.start_term > self.end_term
+	# 		self.errors.add(:base, "Start term may not be after end term.")
+	# 	end
+	# end
 
 end
