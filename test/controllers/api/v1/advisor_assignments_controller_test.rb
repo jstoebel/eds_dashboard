082e412305c709da3f6b6817fff7a32be9d2507d
Jacob Stoebel <stoebelj@berea.edu>
2016-06-17 14:48:03 -0400

    finished test for advisor_assignments happy path

--- test/controllers/api/v1/advisor_assignments_controller_test.rb
+++ test/controllers/api/v1/advisor_assignments_controller_test.rb
@@ -26,16 +26,22 @@ class StudentsControllerTest < ActionController::TestCase
                 :student_id => stu.id,
                 :tep_advisor_id => adv.id
         })
-        patch :update, {:student_id => stu.id, :advisors => []}        
+        patch :update, {:student_id => stu.id, :advisors => []}
         expect assignment_diff.must_equal 0
     end
 
     it "returns http sucess" do
-        patch :update, {:student_id => stu.id, :advisors => []}        
+        patch :update, {:student_id => stu.id, :advisors => []}
         assert_response :created
     end
 
     it "returns a success message" do
+        patch :update, {:student_id => stu.id, :advisors => []}
+        message = JSON.parse(response.body)
+        expect message["success"].must_equal true
+        expect message["msg"].must_equal "Successfully altered advisor assignments."
+        expect message["added"].must_equal []
+        expect message["deleted"].must_equal []
         
     end
 
