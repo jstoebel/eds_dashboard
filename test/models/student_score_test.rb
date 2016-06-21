9fe08b16d8e851d2efd538b25fc4e97819c4e606
cartere <carterebc@gmail.com>
2016-06-21 21:00:58 +0000

    created tests for validations tests in assessment_item_test, assessment_test, assessment_version_test, item_level_test, student_score_test, and student_test

--- test/models/student_score_test.rb
+++ test/models/student_score_test.rb
@@ -4,4 +4,15 @@ class StudentScoreTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "Object not valid, validations fail" do
+    stu_scor=StudentScore.new
+    assert_not stu_scor.valid?
+    assert_equal [:student_id, 
+      :assessment_version_id, 
+      :assessment_item_id, 
+      :item_level_id] ,
+      stu_scor.errors.keys
+    assert_equal [:student_id, :assessment_version_id, :assessment_item_id, :item_level_id].map{|i| [i, ["can't be blank"]]}.to_h, 
+     stu_scor.errors.messages
+  end
 end
