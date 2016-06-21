efa78ca6a36ec007ab23586fa52c1201b65a774a
cartere <carterebc@gmail.com>
2016-06-21 19:42:02 +0000

    created model for student_score

--- db/migrate/20160621193326_create_student_score.rb
+++ db/migrate/20160621193326_create_student_score.rb
@@ -1,6 +1,15 @@
 class CreateStudentScore < ActiveRecord::Migration
-  def change
+  def up
     create_table :student_scores do |t|
+      t.integer :student_id, :null => false
+      t.integer :assessment_version_id, :null => false
+      t.integer :assessment_item_id, :null => false
+      t.integer :item_level_id, :null => false
+      t.timestamps
     end
+  end
+  
+  def down
+    drop_table :student_scores
   end
 end
