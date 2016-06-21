24522a6b4e2fcd3419153beb0958fcd2069b3b90
cartere <carterebc@gmail.com>
2016-06-21 19:15:37 +0000

    added drop migrations

--- db/migrate/20160621190940_drop_assessment_scores.rb
+++ db/migrate/20160621190940_drop_assessment_scores.rb
@@ -1,9 +1,17 @@
 class DropAssessmentScores < ActiveRecord::Migration
   def up
-  
+   drop_table :assessment_scores
   end
-  
+
   def down
-    
+    create_table :assessment_scores do |t|
+      t.integer :student_assessment_id, :null => false
+      t.integer :assessment_item_version_id, :null => false
+      t.integer :score
+      t.timestamps
+    end
+
+    add_foreign_key :assessment_scores, :student_assessments
+    add_foreign_key :assessment_scores, :assessment_item_versions
   end
 end
--- db/migrate/20160621191004_drop_student_assessments.rb
+++ db/migrate/20160621191004_drop_student_assessments.rb
@@ -1,4 +1,15 @@
 class DropStudentAssessments < ActiveRecord::Migration
-  def change
+  def up
+   drop_table :student_assessments
+  end
+   
+  def down
+    create_table :student_assessments do |t|
+      t.integer :student_id, :null => false
+      t.integer :assessment_version_id, :null => false
+      t.timestamps
+    end
+    add_foreign_key :student_assessments, :students
+    add_foreign_key :student_assessments, :assessment_versions
   end
 end
