af06d9f36183164d5d0c62e9424eca995924a88a
Jacob Stoebel <stoebelj@berea.edu>
2016-06-23 16:12:37 -0400

    drafted wells for PRaxis results and issues

--- app/controllers/concern_dashboard_controller.rb
+++ app/controllers/concern_dashboard_controller.rb
@@ -1,22 +1,39 @@
 class ConcernDashboardController < ApplicationController
 
     def index
+
         @student = Student.find params[:student_id]
 
-        if @student.praxis_results.blank?
-            @praxis_status = "info"
-            @praxis_glyph = "question-sign>"
-            @praxis_msg = "not yet attempted"
-        elsif @student.praxisI_pass
-            @praxis_status = "success"
-            @praxis_glyph = "ok-sign"
-            @praxis_msg = "passed"
-        else 
-            @praxis_status = "danger"
-            @praxis_glyph = "alert"
-            @praxis_msg = "not yet passed"
+        @areas = ["praxis", "issues"]
+
+        outcomes = {
+            nil => {:status => "info", :glyph => "question-sign"},
+            true => {:status => "success", :glyph => "ok-sign"}, 
+            false => {:status => "danger", :glyph => "warning-sign"},
+        }
+
+        @areas.each do |area|
+            #get the status in this area
+            status = self.send("#{area}_check", @student)
+            outcome = outcomes[status]
+            outcome.each{|k,v| instance_variable_set("@#{area}_#{k}", v)}
         end
 
+    end
+
+    private
+
+    def praxis_check(stu)
+        if stu.praxis_results.blank?
+            return nil
+        else
+            return stu.praxisI_pass
+        end
+    end
+
+    def issues_check(stu)
+
+        return !    stu.issues.select{|i| i.Open}.present?
 
 
     end
