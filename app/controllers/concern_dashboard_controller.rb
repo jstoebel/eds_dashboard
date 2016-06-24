bdb606bb071251058c7ce1c29e49b8c8457274e1
Jacob Stoebel <stoebelj@berea.edu>
2016-06-24 13:46:42 -0400

    finished draft for concerns controller added ability

--- app/controllers/concern_dashboard_controller.rb
+++ app/controllers/concern_dashboard_controller.rb
@@ -3,20 +3,28 @@ class ConcernDashboardController < ApplicationController
     def index
 
         @student = Student.find params[:student_id]
+        authorize! :be_concerned, @student
 
-        @areas = ["praxis", "issues"]
+        @areas = ["praxis", "issues"]#, "issues"]
 
+        #swap the key out for its value hash.
         outcomes = {
-            nil => {:status => "info", :glyph => "question-sign"},
-            true => {:status => "success", :glyph => "ok-sign"}, 
-            false => {:status => "danger", :glyph => "warning-sign"},
+            nil => {:alert_status => "info", :glyph => "question-sign"},
+            true => {:alert_status => "success", :glyph => "ok-sign"}, 
+            false => {:alert_status => "danger", :glyph => "warning-sign"},
         }
+
+        @concerns = []
 
         @areas.each do |area|
             #get the status in this area
-            status = self.send("#{area}_check", @student)
-            outcome = outcomes[status]
-            outcome.each{|k,v| instance_variable_set("@#{area}_#{k}", v)}
+            args = self.send("#{area}_check", @student)
+            if args.present?
+                outcome = outcomes[args.delete(:outcome)]
+                merged = args.merge outcome
+                @concerns << merged
+            end
+
         end
 
     end
@@ -24,17 +32,26 @@ class ConcernDashboardController < ApplicationController
     private
 
     def praxis_check(stu)
+
+        args = {:title => "Praxis I", :link => student_praxis_results_path(stu.id), :link_title => "View Praxis Results", :link_num => stu.praxis_results.size}
         if stu.praxis_results.blank?
-            return nil
+            return args.merge({:outcome => nil, :msg => "This student has not taken the Praxis I"})
+        elsif stu.praxisI_pass
+            #passed praxis!
+            return args.merge({:outcome => true, :msg => "This student has passed the Praxis I"})
         else
-            return stu.praxisI_pass
+            return args.merge({:outcome => false, :msg => "This student has not passed the Praxis I"})           
         end
     end
 
     def issues_check(stu)
 
-        return !    stu.issues.select{|i| i.Open}.present?
+        if stu.issues.select{|i| i.Open}.present?
+            #student has open issues
+            return {:title => "Advisor Notes", :link => student_issues_path(stu.id), :link_title => "View Advisor Notes", :link_num => stu.issues.open.size,
+                :outcome => false, :msg => "This student has open advisor notes."
 
-
+            }
+        end
     end
 end
