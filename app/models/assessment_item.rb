c7507204f9f2bf353a5d0327144146cc9536669a
cartere <carterebc@gmail.com>
2016-06-27 21:11:08 +0000

    Controller tests done. Index using post instead of get method needs fixed

--- app/models/assessment_item.rb
+++ app/models/assessment_item.rb
@@ -3,6 +3,8 @@
 # Table name: assessment_items
 #
 #  id          :integer          not null, primary key
+#  name  <-- FYI: this is just annotation, it doesn't affect the model. 
+# if its in the db, the model will know about it.
 #  slug        :string(255)
 #  description :text
 #  created_at  :datetime
