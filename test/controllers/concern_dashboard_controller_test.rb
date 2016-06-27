eeee623c8e657e647a037d5a2e18db1ef2a4e810
Jacob Stoebel <stoebelj@berea.edu>
2016-06-27 15:56:38 -0400

    complete tests for concern_dashboard_controller

--- test/controllers/concern_dashboard_controller_test.rb
+++ test/controllers/concern_dashboard_controller_test.rb
@@ -1,23 +1,19 @@
 require 'test_helper'
+require 'application_controller'
 require 'test_teardown'
 class ConcernDashboardControllerTest < ActionController::TestCase
     include TestTeardown
 
     allowed_roles = ["admin", "advisor"]
+    role_names = Role.all.map{|i| i.RoleName}.to_a
 
     before do
         @stu = Student.first
-        # role_names.each do |role|
-        #     usr = instance_variable_set("@#{role}", FactoryGirl.create(role))
-        #     adv = instance_variable_set("@#{role}_adv", (FactoryGirl.create :tep_advisor, {:user_id => usr.id}))
-        #     FactoryGirl.create :advisor_assignment, {:student_id => @stu.id, :tep_advisor_id => adv.id }
-        # end
     end
 
     subject{get :index, :student_id => @stu.id}
     let(:praxis_concern){assigns(:concerns)[:praxis]}
     let(:issue_concern){assigns(:concerns)[:issues]}
-
 
     # FOR PERMITTED ROLES
 
@@ -83,87 +79,16 @@ class ConcernDashboardControllerTest < ActionController::TestCase
             Issue.delete_all
             subject
             expect issue_concern.must_equal nil
-
         end
     end
 
+    (role_names - allowed_roles).each do |r|
 
-
-
-    # describe "index success" do
-
-    #     let(:stu){FactoryGirl.create :student}
-    #     let(:adv_profile){current_user.tep_advisor}
-    #     subject{get :index, :student_id => stu.id}
-
-    #     before do
-    #         FactoryGirl.create :advisor_assignment, {:student_id => stu.id, :tep_advisor_id => adv_profile.id }
-    #     end
-
-    #     it "returns http success" do
-    #         subject
-    #         assert_response :success
-    #     end
-        
-    #     describe "praxis" do
-
-    #         let(:concern){assigns(:concerns)[:praxis]}
-
-    #         it "pulls generic args" do
-    #             subject
-    #             expect concern[:title].must_equal "Praxis I"
-    #             expect concern[:link].must_equal student_praxis_results_path(stu.id)
-    #             expect concern[:link_title].must_equal "View Praxis Results"
-    #             expect concern[:link_num].must_equal stu.praxis_results.size
-    #         end
-
-    #         it "pulls args for passing" do
-    #             pop_praxisI(stu, true)
-    #             subject
-    #             expect concern[:alert_status].must_equal "success"
-    #             expect concern[:glyph].must_equal "ok-sign"
-
-    #         end
-
-    #         it "pulls args for not passing" do
-    #             pop_praxisI(stu, false)
-    #             subject
-    #             expect concern[:alert_status].must_equal "danger"
-    #             expect concern[:glyph].must_equal "warning-sign" 
-    #         end
-
-    #         it "pulls args for no test" do
-    #             subject
-    #             expect concern[:alert_status].must_equal "info"
-    #             expect concern[:glyph].must_equal "question-sign" 
-    #         end
-    #     end #praxis
-
-
-    #     describe "issues" do
-
-    #         let(:concern){assigns(:concerns)[:issues]}
-
-    #         it "pulls args for open issue" do
-                
-    #             FactoryGirl.create :issue, {:student_id => stu.id, :Open => true}
-    #             subject
-    #             expect concern[:title].must_equal "Advisor Notes"
-    #             expect concern[:link].must_equal student_issues_path(stu.id)
-    #             expect concern[:link_title].must_equal "View Advisor Notes"
-    #             expect concern[:link_num].must_equal stu.issues.size
-    #             expect concern[:alert_status].must_equal "danger"
-    #             expect concern[:glyph].must_equal "warning-sign"
-    #         end
-
-    #         it "pulls nothing for no issue" do
-    #             subject
-    #             expect concern.must_equal nil
-    #         end
-
-    #     end
-
-    # end
-
+        it "as #{r} can't index concerns bad role" do
+            load_session r
+            subject
+            assert_redirected_to "/access_denied"
+        end
+    end
 
 end
