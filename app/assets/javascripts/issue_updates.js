8ecf4055905d27e98a5ab1fac709f23816faaf28
Jacob Stoebel <stoebelj@berea.edu>
2016-06-30 17:02:03 -0400

    add broken draf draft for ajax request

--- app/assets/javascripts/issue_updates.js
+++ app/assets/javascripts/issue_updates.js
@@ -1,3 +1,25 @@
+var toggleState = function(updateId, newState) {
+
+    return new Promise(function(resolve, reject){
+
+
+        $.ajax({
+            type: "PATCH", 
+            dataType: "json",
+            //url: "/issue_updates/"+updateId+"?&authenticity_token="+AUTH_TOKEN,
+            data: {issue_update: {addressed : newState}},
+            success: function(response){
+                resolve(response);
+            },
+            error: function(response){
+                reject(response);
+            }
+        })
+    })
+
+
+}
+
 $(document).ready(function(){
 
     //render switches
@@ -10,4 +32,23 @@ $(document).ready(function(){
     $(".bs-switch").bootstrapSwitch('onColor', "default");
     $(".bs-switch").bootstrapSwitch('offColor', "danger");
 
+
+    $(".bs-switch").on('switchChange.bootstrapSwitch', function(event, state){
+        var elemId = event.target.id;
+        var updateId = elemId.match(/\d/)[0];
+        console.log("swtich flipped!")
+        console.log(state);
+        toggleState(updateId, state).then(function(response){
+            console.log("success!");
+            console.log(response);
+        }).catch(function(response){
+            console.log("fail!");
+            console.log(response)
+            console.log(response);
+
+        })
+    })
+
 });
+
+console.log("hello");
