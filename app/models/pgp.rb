bf243655df4358e8104768de6a740816d518a85f
wisemanh <wisemanh@berea.edu>
2016-06-30 14:51:58 +0000

    Score moved to details, extra validations, details view changes

--- app/models/pgp.rb
+++ app/models/pgp.rb
@@ -13,4 +13,8 @@ class Pgp < ActiveRecord::Base
         presence: {message:"Please enter a description."}
     validates :plan,
         presence: {message:"Please enter a plan."}
+    validates_numericality_of :goal_score, greater_than: 0
+    validates_numericality_of :goal_score, less_than: 5
+    validates :score_reason,
+        presence: {message: "Please enter a score reason."}
 end
