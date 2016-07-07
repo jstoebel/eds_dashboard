2f727b5ccb8850bd882531f5670f8befa584b90b
wisemanh <wisemanh@berea.edu>
2016-07-07 06:00:09 +0000

    schema change

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160629202930) do
+ActiveRecord::Schema.define(version: 20160630150652) do
 
   create_table "adm_st", force: true do |t|
     t.integer  "student_id",                       null: false
