80981ef334838ed889cfcd8bfc4aaecafe44442a
cartere <carterebc@gmail.com>
2016-07-12 19:57:14 +0000

    Added get new test to assessment_versions_controller_test

--- test/models/assessment_test.rb
+++ test/models/assessment_test.rb
@@ -21,5 +21,23 @@ class AssessmentTest < ActiveSupport::TestCase
       assess.errors.messages
   end
   
-  test
+  test "versions" do
+    ver = FactoryGirl.create :version_with_items
+    assess = ver.assessment
+    assert_equal assess.versions, assess.assessment_versions 
+  end
+  
+  test "current_version" do
+    ver = FactoryGirl.create :version_with_items
+    assess = ver.assessment
+    current = assess.versions.order(:created_at => :asc).last
+    assert_equal assess.current_version, current
+  end
+  
+  test "has_scores" do
+    ver = FactoryGirl.create :version_with_items
+    assess = ver.assessment
+    score = assess.assessment_versions.select { |v| v.student_scores.present?}.size > 0
+    assert_equal assess.has_scores, score
+  end
 end
