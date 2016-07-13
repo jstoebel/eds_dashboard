820581a8ce94fc4043eb8e9d1fc0bbe44175c403
cartere <carterebc@gmail.com>
2016-07-13 20:58:24 +0000

    Test version controller actions -Get index and get index for versions of one assessment test -get edit test

--- test/controllers/assessment_versions_controller_test.rb
+++ test/controllers/assessment_versions_controller_test.rb
@@ -2,8 +2,32 @@ require 'test_helper'
 
 class AssessmentVersionsControllerTest < ActionController::TestCase
   allowed_roles = ["admin", "staff"]
+  
+  def assert_form_setup
+    assert_equal assigns(:assessments), Assessment.all
+    assert_equal assigns(:items), AssessmentItem.all
+  end
+  
+  test "should get index for versions of same assessment" do
+    assess = FactoryGirl.create :assessment
+    allowed_roles.each do |r|
+      load_session(r)
+      get :index, params: {assessment_id: assess.id}
+      assert_response :success
+      assert_equal assigns(:version), AssessmentVersion.where(:assessment_id => assess.id)
+    end
+  end
+  
+  test "should get index" do
+    allowed_roles.each do |r|
+      load_session(r)
+      get :index
+      assert_response :success
+      assert_equal assigns(:version), AssessmentVersion.all
+    end
+  end
+  
   test "should get new" do
-    
     allowed_roles.each do |r|
       load_session(r)
       assess = FactoryGirl.create(:assessment)
@@ -15,13 +39,14 @@ class AssessmentVersionsControllerTest < ActionController::TestCase
     end
   end
 
-  test "should get create" do
+  test "should post create" do
     allowed_roles.each do |r|
       load_session(r)
       assess = FactoryGirl.create(:assessment)
       version = AssessmentVersion.new
-      create_params = {:assessment_id => assess.id}
+      create_params = {assessment_version => {:assessment_id => assess.id}}
       post :create, create_params
+      assert_response :success
       assessment_params = create_params[:assessment_version]
       expected_assessment = AssessmentVersion.new assessment_params
       assert assigns(:assessment).valid?, assigns(:assessment).inspect
@@ -29,7 +54,7 @@ class AssessmentVersionsControllerTest < ActionController::TestCase
     end
   end 
   
-  test "should get delete" do
+  test "should delete" do
     allowed_roles.each do |r|
       load_session(r)
       assess = FactoryGirl.create :assessment
@@ -42,24 +67,40 @@ class AssessmentVersionsControllerTest < ActionController::TestCase
     end
   end
 
-  test "should get destroy" do
-    get :destroy
-    assert_response :success
-  end
-
-  test "should get update" do
-    get :update
-    assert_response :success
+  test "should not delete bad role" do
+    (role_names - allowed_roles).each do |r|
+      load_session(r)
+      assess = FactoryGirl.create :assessment
+      version = FactoryGirl.create :assessment_version
+      post :delete, {:assessment_version_id => version.id}
+      assert_equal flash[:notice], "Record cannot be deleted"
+      assert_redirected_to assessment_assessment_versions_path(assess.id)
+    end
   end
 
   test "should get edit" do
-    get :edit
-    assert_response :success
+    allowed_roles.each do |r|
+      load_session(r)
+      version = FactoryGirl.create :assessment_version
+      get :edit, :id => version.id
+      assert_response :success
+      assert_form_setup
+      assert_equal assigns(:version), version
+    end
   end
 
-  test "should get index" do
-    get :index
-    assert_response :success
-  end
+  test "should post update" do
 
+    allowed_roles.each do |r|
+      load_session(r)
+      version = FactoryGirl.create :assessment_version
+      item = FactoryGirl.create :assessment_item
+      version.assessment_items.id = item
+      #edit_params = {:assessment_items => AssessmentItem.find(item.id)}}
+      post :update, {:id => version.id, :assessment_version => edit_params}
+      assert_redirected_to(assessment_versions_path)
+      assert_equal flash[:notice], "Updated Version #{version.version_num} of #{Assessment.find(version.assessment_id).name}"
+      assert_equal assigns(:version), version
+    end
+  end
 end
