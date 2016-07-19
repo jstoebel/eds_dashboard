d097608de365bb7e4fded22aa1c5bde1fcbc99a4
cartere <carterebc@gmail.com>
2016-07-19 18:57:22 +0000

    Complete assessment_item update action -Add check_scores and has_scores? to item_level model to avoid deleting parent levels -Add match update route so that assessment_item id was not in url and can be passed in json

--- app/controllers/assessment_items_controller.rb
+++ app/controllers/assessment_items_controller.rb
@@ -27,24 +27,27 @@ class AssessmentItemsController < ApplicationController
   end
 
   def update
-
     # update an assessment item
       # the models needs to validate that the item is not currently associated
       # with any assessments that have any scores. See Jacob for questions.
     # based on levels provided add and remove as needed
-    # to edit the content of an item_level see the item_levels controller
-    puts params.inspect
-    puts params.class
-    @item = AssessmentItem.find(params[:id])
+    # # to edit the content of an item_level see the item_levels controller
+    
+    new_params = update_params.to_h    #from ActionController::Parameters to hash
+    puts new_params.inspect
+    @item = AssessmentItem.find(new_params["id"])
     old_levels = @item.item_levels    #active record collection. map to convert to arry
+    convert_levels = old_levels.map {|l| l.attributes}    #array of hashes
+
     if @item.has_scores?
       render json: @item.errors.full_messages, status: :unprocessable_entity
     else
-      @item.update_attributes(update_params)
-      convert_levels = old_levels.map {|l| l.attributes}    #array
-      new_levels = JSON.parse(request.raw_post, :quirks_mode => true)["assessment_item"]["item_level_attributes"]    #array
+      @item.update_attributes(new_params)
+      new_levels = JSON.parse(request.raw_post, :quirks_mode => true)["assessment_item"]["item_levels"]    #array of hashes
+      
       (new_levels - convert_levels).each{|l| ItemLevel.create(l)}
-      (convert_levels - new_levels).each { |l| ItemLevel.find(l["id"]).destroy }
+      (convert_levels - new_levels).each{ |l| ItemLevel.find(l["id"]).destroy}
+      
       if @item.save
         render json: @item, status: :ok
       else
@@ -63,11 +66,13 @@ class AssessmentItemsController < ApplicationController
     params.require(:assessment_item).permit(:slug, :description, :name)
   end
   
-  def level_params
-    params.require(:assessment_item).permit(:item_levels => [:assessment_item_id, :descriptor, :level, :ord])
+  def level_params(lvl)
+    lvl.require(:assessment_item).permit(:item_levels => [:assessment_item_id, :descriptor, :level, :ord])
+    #params.require(:assessment_item).permit(:item_levels => [:assessment_item_id, :descriptor, :level, :ord])
   end
   
   def update_params
-    params.require(:assessment_item).permit(:slug, :description, :name)
+    #item.permit(:id, :slug, :description, :name)
+    params.require(:assessment_item).permit(:id, :slug, :description, :name, :item_levels)
   end
 end
--- app/controllers/item_levels_controller.rb
+++ app/controllers/item_levels_controller.rb
@@ -6,10 +6,18 @@ class ItemLevelsController < ApplicationController
     @level = ItemLevel.new(create_params)
     @item = AssessmentItem.find(params[:assessment_item_id])
     authorize! :manage, @level
-
   end
 
-  def delete
+  def destroy
+    @level = ItemLevel.find(params[:id])
+    @item = AssessmentItem.find(params[:assessment_item_id])
+    authorize! :manage, @level
+    if @level.has_scores?
+      render json: @level.errors.full_messages, status: :unprocessable_entity
+    else 
+      @level.destroy 
+      render json: @item, status: :ok
+    end
   end
   
   private
