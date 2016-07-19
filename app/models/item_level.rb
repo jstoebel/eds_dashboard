b3e31c502cb249a3e55bebcb63a82e2b7bcacf98
cartere <carterebc@gmail.com>
2016-07-19 18:57:22 +0000

    Complete assessment_item update action -Add check_scores and has_scores? to item_level model to avoid deleting parent levels -Add match update route so that assessment_item id was not in url and can be passed in json

--- app/models/item_level.rb
+++ app/models/item_level.rb
@@ -23,4 +23,19 @@ class ItemLevel < ActiveRecord::Base
     belongs_to :assessment_item
 
     validates_presence_of :descriptor, :level, :assessment_item_id
+        
+    before_validation :check_scores #test that does stop, that doesn't when shouldn't
+    before_destroy :check_scores
+    
+    def has_scores?
+      return self.student_scores.present?
+    end
+    
+    private
+    def check_scores
+        if self.has_scores?    #if true
+          self.errors.add(:base, "Can't delete level. Has associated scores.")   
+          return false
+      end
+  end
 end
