f8e2a8f46c22b429765366b7ac5ae54bf0568d6c
cartere <carterebc@gmail.com>
2016-07-20 17:03:59 +0000

    Complete update action for assessment_item -Use params, whitelist each attribute before updating and creating levels

--- test/controllers/assessment_items_controller_test.rb
+++ test/controllers/assessment_items_controller_test.rb
@@ -12,38 +12,38 @@ class AssessmentItemsControllerTest < ActionController::TestCase
         end
     end
     
-    test "Update - Old levels deleted if not similar, new levels added" do
+    test "Update - delete old levels, no scores" do
       allowed_roles.each do |r|
         load_session(r)
         item = FactoryGirl.create :assessment_item
+        puts item.item_levels.inspect
         old_lvl = FactoryGirl.create :item_level, {:assessment_item_id => item.id}
-        
         update_params = {
             "assessment_item" => {
                 "id" => "#{item.id}",
-                "slug"=> "NewSlug", 
+                "slug"=> "NewSlug",
                 "description" => "New description", 
-                "name" => "New Name", 
-                "item_levels" => [{
+                "name" => "New Name",
+                "item_levels_attributes" => [{
                     "descriptor" => "descriptor", 
                     "level" => "Word String", 
                     "ord" => "3"}]
             }
         }
-
-        patch :update, {format: :json, :assessment_item => update_params}
+        patch :update, update_params #{format: :json}
         assert_response :success
         assert_equal assigns(:item), item
-        assert_equal assigns(:item.item_levels.attributes), update_params[:assessment_item][:item_level_attributes]
+        
+        
+        #assert old_lvl.destroyed?
+        puts update_params[:assessment_item].inspect
+        assert_equal assigns(:item).item_levels.map{|i| i.attributes}, update_params[:assessment_item][:item_level_attributes]
+        #assert new created
+        #puts "here are the attributes"
+        #attri = 
+        #puts attri.inspect
+        #assert_equal assigns(:item).item_levels.map{|k| k.attributes}, 
         assert_equal @response.body, assigns(:item).to_json
       end
     end
-    
-=begin    private
-    def raw_post(action, params, body)
-      @request.env['RAW_POST_DATA'] = body
-      response = patch(action, params)
-      @request.env.delete('RAW_POST_DATA')
-      response
-=end
 end
