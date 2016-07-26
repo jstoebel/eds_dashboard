e4725abb91d8d5ac89e2759d1c3524f6f6a28aa1
wisemanh <wisemanh@berea.edu>
2016-07-26 21:02:34 +0000

    Forms for edit scores and edit pgp now working, delete icon disabled if has scores, tests for pgp controller

---
+++ test/controllers/pgps_controller_test.rb
@@ -0,0 +1,93 @@
+require 'test_helper'
+
+class PgpsControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # 
+  
+  allowed_roles = ["admin", "staff"] 
+  
+  test "get index" do
+    allowed_roles.each do |r|
+      load_session(r)
+      stu = FactoryGirl.create :student
+      pgp = FactoryGirl.create :pgp
+      
+      get :index, {:student_id => stu.id}
+      assert_response :success
+      
+    end
+  end
+  
+  test "index bad role" do
+    (roles - allowed_roles).each do |r|
+      load_session(r)
+      stu = FactoryGirl.create :student
+      pgp = FactoryGirl.create :pgp
+      
+      get :index, {:student_id => stu.id}
+    end
+  end
+      
+  
+  test "bad role shoud not create pgp" do 
+    (roles - allowed_roles).each do |r|
+      load_session(r)
+      stu = FactoryGirl.create :student
+      pgp = FactoryGirl.create :pgp
+      post :create, {:student_id => stu.id}
+      assert_redirected_to student_pgps_path
+      assert_equal assigns(:pgp), pgp.id
+      assert_equal flash[:notice], "Error creating professional growth plan."
+    end
+  end
+  
+  test "should not edit - bad role" do 
+    (roles - allowed_roles).each do |r|
+      load_session(r)
+    stu = FactoryGirl.create :student
+    pgp = FactoryGirl.create :pgp
+    post :edit, {:pgp => pgp.id}
+    assert_equal flash[:notice] = "Error in editing PGP."
+    end
+  end
+  
+  test "Should Delete PGP" do 
+    allowed_roles.each do |r|
+      load_session(r)
+    stu = FactoryGirl.create :student
+    pgp = FactoryGirl.create :pgp
+    post :destroy, {:id => pgp.id}
+    assert_equal(assigns(:pgp), pgp)
+    assert assigns(:pgp).destroyed?
+    assert_equal flash[:notice], "Professional growth plan deleted successfully"
+    assert_redirected_to student_pgps_path(assigns(:pgp).student_id)
+    end
+  end
+  
+  test "Should not delete - bad role" do 
+    (roles - allowed_roles).each do |r|
+      load_session(r)
+    stu = FactoryGirl.create :student
+    pgp = FactoryGirl.create :pgp 
+    post :destroy, {:id => pgp.id}
+    assert_equal flash[:notice], "Professional growth plan unable to be deleted due to being scored"
+    assert_redirected_to student_pgps_path(assigns(:pgp).student_id)
+    end
+  end
+  
+  test "shouldn't delete - has scores" do 
+    allowed_roles.each do |r|
+      load_session(r)
+    stu = FactoryGirl.create :student
+    pgp = FactoryGirl.create :pgp
+    scores = FactoryGirl.create :pgp_score
+    post :destroy, {:id => pgp.id}
+    assert_equal flash[:notice], "Unable to alter due to scoring "
+    end
+  end
+    
+    
+  
+  
+end
