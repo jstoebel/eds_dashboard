c5a0e8627e159d898829f91f6702834b0f92b31a
cartere <carterebc@gmail.com>
2016-07-27 20:04:34 +0000

    Complete assessment_item controller tests -Test for allowed_roles

--- test/controllers/assessment_items_controller_test.rb
+++ test/controllers/assessment_items_controller_test.rb
@@ -33,7 +33,7 @@ class AssessmentItemsControllerTest < ActionController::TestCase
             create_params = {:slug => "test slug", :description => "test description", :name => "test name"} 
             post :create, {:assessment_item => create_params}
             assert assigns(:item).valid?
-            assert_response :created    
+            assert_response :created
         end
     end
     
@@ -49,7 +49,7 @@ class AssessmentItemsControllerTest < ActionController::TestCase
         end
     end
     
-    test "Should delete item" do
+    test "Should destroy item" do
         allowed_roles.each do |r|
             load_session(r)
             item = FactoryGirl.create :assessment_item
@@ -60,45 +60,111 @@ class AssessmentItemsControllerTest < ActionController::TestCase
         end
     end
     
-    test "Should delete item and associated levels" do
+    test "Should destroy item and associated levels" do
         ##item.item_levels.inspect gives empty Collection, but level.assessment_item_id == item.id
+        ##find out why
         allowed_roles.each do |r|
             load_session(r)
             level = FactoryGirl.create :item_level
             item = level.assessment_item
             post :destroy, {:id => item.id}
             assert_equal item, assigns(:item)
+            #assert item.item_levels.size == 1
             assert_equal item.item_levels, assigns(:item).item_levels
             assert assigns(:item).destroyed?
             assert assigns(:item).item_levels.each{ |l| l.destroyed? }
             assert_response :no_content
         end
     end
-    
-    ##Bad params
-    test "Should not patch update, has scores" do
-    end
-    
-        
-    test "Should not get index, bad params" do
-    end
-    
-    test "Should not get show, bad params" do 
-    end
-        
-    test "Should not patch update, bad params" do 
-    end
-    
-    
+       
     ###Bad Roles
     test "Should not get index, bad role" do
+        version = FactoryGirl.create :version_with_items
+        (role_names - allowed_roles).each do |r|
+            load_session(r)
+            get :index, :assessment_version_id => version.id
+            assert_redirected_to "/access_denied"
+        end
     end
     
-    test "Should not get show, bad role" do 
+    test "Should not get show, bad role" do
+        item = FactoryGirl.create :assessment_item
+        (role_names - allowed_roles).each do |r|
+            load_session(r)
+            get :show, :id => item.id
+            assert_redirected_to "/access_denied"
+        end
     end
     
-    test "Should not patch update, bad role" do 
+    test "Should not post create, bad role" do
+        (role_names - allowed_roles).each do |r|
+            load_session(r)
+            create_params = {:slug => "test slug", :description => "test description", :name => "test name"} 
+            post :create, {:assessment_item => create_params}
+            assert_redirected_to "/access_denied"
+        end
     end
-
-
+    
+    test "Should not patch update, bad role" do
+        item = FactoryGirl.create :assessment_item
+        (role_names - allowed_roles).each do |r|
+            load_session(r)
+            patch :update, :id => item.id
+            assert_redirected_to "/access_denied"
+        end
+    end
+    
+    test "Should not destroy, bad role" do
+        item = FactoryGirl.create :assessment_item
+        (role_names - allowed_roles).each do |r|
+            load_session(r)
+            post :destroy, :id => item.id
+            assert_redirected_to "/access_denied"
+        end
+    end
+    
+    ##Bad params
+    test "Should not post create, bad params" do
+        allowed_roles.each do |r|
+            load_session(r)
+            create_params = {:slug => nil, :description => "test description", :name => "test name"} 
+            post :create, {:assessment_item => create_params}
+            assert_not assigns(:item).valid?
+            assert_response :unprocessable_entity
+        end
+    end
+    
+    test "Should not patch update, bad params" do
+        allowed_roles.each do |r|
+            load_session(r)
+            item = FactoryGirl.create :assessment_item
+            update_params = {:id => item.id, :slug => nil}
+            patch :update, {:assessment_item => update_params}
+            assert_equal item, assigns(:item)
+            assert_response :unprocessable_entity
+        end
+    end
+    
+    test "Should not patch update, has scores" do
+        allowed_roles.each do |r|
+            load_session(r)
+            item = FactoryGirl.create(:version_with_items).assessment_items.first
+            update_params = {:id =>item.id, :slug => "new slug test"}
+            patch :update, {:assessment_item => update_params}
+            assert_equal item, assigns(:item)
+            assert_not assigns(:item).valid?
+            assert_response :unprocessable_entity
+        end
+    end
+        
+    test "Should not destroy, has scores" do
+        allowed_roles.each do |r|
+            load_session(r)
+            item = FactoryGirl.create(:version_with_items).assessment_items.first
+            post :destroy, :id => item.id
+            assert_equal item, assigns(:item)
+            assert assigns(:item).present?
+            assert_response :unprocessable_entity
+        end
+    end
 end
