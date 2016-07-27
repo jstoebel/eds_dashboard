9719b14a163ad1cc71a1022dc38e7f605aa27831
cartere <carterebc@gmail.com>
2016-07-27 18:02:57 +0000

    Finish assessment_item controller actions -Add destroy, create, and update. -Change status rendered in show and index to :ok instead of :success

--- test/controllers/assessment_items_controller_test.rb
+++ test/controllers/assessment_items_controller_test.rb
@@ -12,7 +12,7 @@ class AssessmentItemsControllerTest < ActionController::TestCase
             get :index, :assessment_version_id => version.id
             assert_equal assigns(:item), version.assessment_items.sorted
             assert_equal @response.body, assigns(:item).to_json
-            assert_response :success
+            assert_response :ok
         end
     end
     
@@ -23,31 +23,63 @@ class AssessmentItemsControllerTest < ActionController::TestCase
             get :show, :id => item.id
             assert_equal item, assigns(:item)
             assert_equal @response.body, assigns(:item).to_json
-            assert_response :success
+            assert_response :ok
         end
     end
     
     test "Should post create" do
-        
-        
+        allowed_roles.each do |r|
+            load_session(r)
+            create_params = {:slug => "test slug", :description => "test description", :name => "test name"} 
+            post :create, {:assessment_item => create_params}
+            assert assigns(:item).valid?
+            assert_response :created    
+        end
     end
     
     test "Should patch update" do
-    
+        allowed_roles.each do |r|
+            load_session(r)
+            item = FactoryGirl.create :assessment_item
+            update_params = {:id => item.id , :slug => "test slug", :description => "test description", :name => "test name"}
+            patch :update, {:assessment_item => update_params}
+            assert assigns(:item).valid?
+            assert_equal item, assigns(:item)
+            assert_response :ok
+        end
     end
     
+    test "Should delete item" do
+        allowed_roles.each do |r|
+            load_session(r)
+            item = FactoryGirl.create :assessment_item
+            post :destroy, {:id => item.id}
+            assert_equal item, assigns(:item)
+            assert assigns(:item).destroyed?
+            assert_response :no_content
+        end
+    end
+    
+    test "Should delete item and associated levels" do
+        ##item.item_levels.inspect gives empty Collection, but level.assessment_item_id == item.id
+        allowed_roles.each do |r|
+            load_session(r)
+            level = FactoryGirl.create :item_level
+            item = level.assessment_item
+            post :destroy, {:id => item.id}
+            assert_equal item, assigns(:item)
+            assert_equal item.item_levels, assigns(:item).item_levels
+            assert assigns(:item).destroyed?
+            assert assigns(:item).item_levels.each{ |l| l.destroyed? }
+            assert_response :no_content
+        end
+    end
+    
+    ##Bad params
     test "Should not patch update, has scores" do
     end
     
-    test "Should not get index, bad role" do
-    end
-    
-    test "Should not get show, bad role" do 
-    end
-    
-    test "Should not patch update, bad role" do 
-    end
-    
+        
     test "Should not get index, bad params" do
     end
     
@@ -56,5 +88,17 @@ class AssessmentItemsControllerTest < ActionController::TestCase
         
     test "Should not patch update, bad params" do 
     end
+    
+    
+    ###Bad Roles
+    test "Should not get index, bad role" do
+    end
+    
+    test "Should not get show, bad role" do 
+    end
+    
+    test "Should not patch update, bad role" do 
+    end
+
 
 end
