52445c56cbddc78f2ff8db79708ac260adfb91eb
wisemanh <wisemanh@berea.edu>
2016-07-29 18:18:12 +0000

    prog status chagned so that transferred and graduated come back as not applying

--- app/models/student.rb
+++ app/models/student.rb
@@ -229,9 +229,10 @@ class Student < ActiveRecord::Base
 
 		# Not applying: any of the following
 		# 	A student who has indicated they are not interested in applying to the TEP. OR
-		# 	A student who was dismissed from the college and never aditted to TEP
+		# 	A student who was dismissed from the college and never admitted to TEP
 		elsif 	(self.latest_foi.present? and not self.latest_foi.seek_cert) or
-				(self.was_dismissed?)
+				(self.was_dismissed?) or
+				graduated or transfered
 			return "Not applying"
 
 		# Candidate
