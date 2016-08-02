7f7aaa5bacd2a83825feb1a3bd3a458bbc50372f
cartere <carterebc@gmail.com>
2016-08-02 17:00:06 +0000

    Begin model tests

--- test/models/item_level_test.rb
+++ test/models/item_level_test.rb
@@ -18,12 +18,26 @@ class ItemLevelTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
-  test "Object not valid, validations failed" do
-    lev = ItemLevel.new
+  
+  test "Parameters not valid, validations failed" do
+    lev = FactoryGirl.create(:item_level, {:descriptor => nil, :level => nil})
     assert_not lev.valid?    #not a valid object
     #error due to all required attributes?
     assert_equal [:descriptor, :level, :assessment_item_id], lev.errors.keys
     assert_equal [:descriptor, :level, :assessment_item_id].map{|i| [i, ["can't be blank"]]}.to_h, 
       lev.errors.messages
   end
+  
+  test "Sorted scope" do
+    num_lvl = 3
+    levels = FactoryGirl.create_list(:item_level, num_lvl)
+    ordered_lvls = levels.sort_by{ |l| l.ord}
+    assert_equal ordered_lvls, ItemLevel.sorted
+  end
+  
+  test "validation of descriptor" do
+    level = ItemLevel.new
+    
+  end
+  
 end
