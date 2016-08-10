76fe0b05f7c64bdc5c87aa0ed3d473db3ae03cea
Jacob Stoebel <stoebelj@berea.edu>
2016-08-10 14:15:43 -0400

    finish tests for baner_update mailers

--- test/mailers/banner_update_mailer_test.rb
+++ test/mailers/banner_update_mailer_test.rb
@@ -9,7 +9,7 @@ class BannerUpdateMailerTest < ActionMailer::TestCase
       assignment = FactoryGirl.create :advisor_assignment
       @stu = assignment.student
       @adv = assignment.tep_advisor
-      @email = BannerUpdateMailer.notify_possible_drop(assignment.student, assignment.tep_advisor)
+      @email = BannerUpdateMailer.possible_drop(assignment.student, assignment.tep_advisor)
 
       assert_emails 1 do
         @email.deliver_now
@@ -21,6 +21,43 @@ class BannerUpdateMailerTest < ActionMailer::TestCase
       assert_equal @email.cc, ["stoebelj@berea.edu", "rosenbarkerl@berea.edu"]
       assert_equal @email.subject, "Possible TEP status change for #{@stu.name_readable}"
 
+    end
+
+  end
+
+  describe "notify add_drop advisor" do
+
+    before do
+      assignment = FactoryGirl.create :advisor_assignment
+      @stu = assignment.student
+      @adv = assignment.tep_advisor
+    end
+
+    it "notifies advisee add" do
+      @email = BannerUpdateMailer.add_drop_advisor(@stu, @adv)
+
+      assert_emails 1 do
+        @email.deliver_now
+      end
+
+      assert_equal @email.to, [@adv.get_email]
+      assert_equal @email.cc, ["stoebelj@berea.edu", "rosenbarkerl@berea.edu"]
+      assert_equal @email.subject, "Advisee status change for #{@stu.name_readable}"
+      assert_match /added/, @email.body.to_s
+    end
+
+    it "notifies advisee remove" do
+      AdvisorAssignment.delete_all
+      @email = BannerUpdateMailer.add_drop_advisor(@stu, @adv)
+
+      assert_emails 1 do
+        @email.deliver_now
+      end
+
+      assert_equal @email.to, [@adv.get_email]
+      assert_equal @email.cc, ["stoebelj@berea.edu", "rosenbarkerl@berea.edu"]
+      assert_equal @email.subject, "Advisee status change for #{@stu.name_readable}"
+      assert_match /removed/, @email.body.to_s
     end
 
   end
