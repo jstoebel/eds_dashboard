0d6865c4d9f7eeb00b70947d40aac7cd5896556c
Jacob Stoebel <stoebelj@berea.edu>
2016-08-11 14:42:06 -0400

    finished banner_update!

--- app/mailers/banner_update_mailer.rb
+++ app/mailers/banner_update_mailer.rb
@@ -7,7 +7,7 @@ class BannerUpdateMailer < ApplicationMailer
     @stu = stu
     @adv = adv
     mail(:to => adv.get_email,
-      :cc => ["stoebelj@berea.edu", "rosenbarkerl@berea.edu"],
+      :cc => [SECRET["APP_ADMIN_EMAIL"], "rosenbarkerl@berea.edu"],
       :subject => "Possible TEP status change for #{@stu.name_readable}")
   end
 
@@ -19,8 +19,19 @@ class BannerUpdateMailer < ApplicationMailer
     @stu = stu
     @adv = adv
     mail(:to => adv.get_email,
-      :cc => ["stoebelj@berea.edu", "rosenbarkerl@berea.edu"],
+      :cc => [SECRET["APP_ADMIN_EMAIL"], "rosenbarkerl@berea.edu"],
       :subject => "Advisee status change for #{@stu.name_readable}")
+  end
+
+  def update_done(timestamp, error_count)
+    # timestamp (datetime): time of task execution
+    # error_count(int) number of caught errors.
+
+    @timestamp = timestamp
+    @error_count = error_count
+    mail(:to => SECRET["APP_ADMIN_EMAIL"],
+      :subject => "banner_update task finished successfully"
+    )
   end
 
 end
