1d64f0db5f9faa1f415541ef704410623872a23b
wisemanh <wisemanh@berea.edu>
2016-08-17 18:50:46 +0000

    Added a different logic for hashing table values

--- app/controllers/fois_controller.rb
+++ app/controllers/fois_controller.rb
@@ -6,9 +6,14 @@ class FoisController < ApplicationController
     def index 
         @fois = Foi.all
     end
-    
+
+  
     def import
-        
+        Foi.import(params[:file])
+        redirect_to fois_path, notice: "Fois imported."
     end
     
+    def create
+       Foi.new 
+    end
 end
--- app/models/foi.rb
+++ app/models/foi.rb
@@ -23,16 +23,33 @@ class Foi < ActiveRecord::Base
 
   
   def self.import(foi_file)
-    file_path = "foi_file_test.csv"
+  
+    CSV.foreach(foi_file.path) do |row|
+      #i = $.
+      #attribute_array = [{:bnum => 'Q1.2_3 - B#', :FirstName => 'Q1.2_1 - First Name', :LastName => 'Q1.2_2 - Last Name'}]
+      #attrs = [:Bnum, :FirstName, :LastName]
+      # h = ["Q1.2_3 - B#", "Q1.2_1 - First Name", "Q1.2_2 - Last Name"]
+      # csv_hash = attrs.zip(h).to_h
+      a = ["Q1.2_3 - B#", "Q1.2_1 - First Name", "Q1.2_2 - Last Name"]
+      b = [row[8][2], row[6][2], row[7][2]]
+      c = [:Bnum, :FirstName, :LastName]
+      attribute_array = Hash[a.zip b]
+      attrs_array = Hash[c.zip a]
+      # b_num = row[8][2]
+      # first_name = row[6][2]
+      # last_name = row[7][2]
+      # attribute_array.push(["Q1.2_3 - B#", b_num], ["Q1.2_1 - First Name", first_name], ["Q1.2_2 - Last Name", last_name]).to_h
+      
     
-    CSV.foreach(file_path) do |row|
-      i = $.
-      attribute_array = [{:bnum => 'Q1.2_3 - B#', :FirstName => 'Q1.2_1 - First Name', :LastName => 'Q1.2_2 - Last Name'}]
-      if i == 1
-        header = row.to_h.slice(attribute_array)
-      elsif i > 1
-        attribute_array.push
-      end
+      #param_hash = [{:Bnum => "Q1.2_3 - B#", :FirstName => "Q1.2_1 - First Name", :LastName => "Q1.2_2 - Last Name"}]
+      
+      
+      if Student.where(Bnum: row[8][2]) == 1
+        stu_id = Student.find_by(Bnum: row[8][2]).id
+        attribute_array.push(["student_id", stu_id])
+        parameters = ActionController::Parameters.new(param_hash)
+        Foi.create(parameters.permit(:student_id, :Bnum, :FirstName, :LastName))
+      end 
       
     end
     
