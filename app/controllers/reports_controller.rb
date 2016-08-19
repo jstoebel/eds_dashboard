e0ea1e6650ed5c8062b8c874914f1a82399df9b0
Shadia <praters@berea.edu>
2016-08-19 18:02:31 +0000

    Reports_controller fixed, working on display

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -7,20 +7,8 @@ class ReportsController < ApplicationController
     authorize_resource
     
     def index
-        @report = Report.find params[:id]
-        authorize! :read, @report
-        @scores = @report.student_scores.select{|r| can? :read, r}
-        authorize! :read, @scores
-        respond_to do |format|
-          format.html
-          format.xls
-        end
+        @student = Student.all.by_last.current
+        authorize! :show, @student
     end
-    
-    def show
-       @report = Report.find params[:id]
-       authorize! :show, @report
-    end
-    
     
 end
