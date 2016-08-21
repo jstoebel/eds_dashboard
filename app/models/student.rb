d5a85b93fb656881c7ae5934456b4cd332146dce
Jacob Stoebel <stoebelj@berea.edu>
2016-08-21 10:10:35 -0400

    tests for new students#index logic

--- app/models/student.rb
+++ app/models/student.rb
@@ -94,7 +94,9 @@ class Student < ActiveRecord::Base
 	scope :current, lambda {select {|s| ["Candidate", "Prospective"].include?(s.prog_status) }}
 	scope :candidates, lambda {select {|s| ["Candidate"].include?(s.prog_status) }}
 
-	scope :with_name, ->(name) {where("FirstName=? or PreferredFirst=? or LastName=? or PrevLast=?", name, name, name, name)}
+
+	# TODO: add support for searching through previous last names
+	scope :with_name, ->(name) {where("FirstName=? or PreferredFirst=? or LastName=?", name, name, name)}
 
 	def self.batch_create(hashes)
 		#bulk inserts student records
