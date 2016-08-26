03277aff34c0ac17be0d0f8a3cebd1533a34ebde
Jacob Stoebel <stoebelj@berea.edu>
2016-08-26 10:50:20 -0400

    cut score handled exclusivly by praxis_test
    the method PraxisResult.cut_score now references the CutScore on the results praxis_test

--- test/services/praxis_score_report_service_test.rb
+++ test/services/praxis_score_report_service_test.rb
@@ -1,15 +1,15 @@
 require 'test_helper'
 
 class ProcessStudentServiceTest < ActiveSupport::TestCase
-
-  before do
-    doc = Nokogiri::XML(Rails.root.join("test", "praxis_report_sample.xml"))
-    report = doc.root
-    @test_service = PraxisScoreReport.new report
-  end
-
-  test "playground" do
-    puts @test_service.report.at_xpath('scorereport/candidateinfo/ssn').text
-  end
+  # 
+  # before do
+  #   doc = Nokogiri::XML(Rails.root.join("test", "praxis_report_sample.xml"))
+  #   report = doc.root
+  #   @test_service = PraxisScoreReport.new report
+  # end
+  #
+  # test "playground" do
+  #   puts @test_service.report.at_xpath('scorereport/candidateinfo/ssn').text
+  # end
 
 end
