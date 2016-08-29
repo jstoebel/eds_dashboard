99e6903a640ee3b680ad9a2e0e1712d1672a980d
Jacob Stoebel <stoebelj@berea.edu>
2016-08-29 12:51:04 -0400

    draft for finalize method

--- test/factories/praxis_sub_temps.rb
+++ test/factories/praxis_sub_temps.rb
@@ -1,5 +1,26 @@
+# == Schema Information
+#
+# Table name: praxis_sub_temps
+#
+#  id                    :integer          not null, primary key
+#  praxis_result_temp_id :integer          not null
+#  sub_number            :integer
+#  name                  :string(255)
+#  pts_earned            :integer
+#  pts_aval              :integer
+#  avg_high              :integer
+#  avg_low               :integer
+#
+
+include Faker
 FactoryGirl.define do
   factory :praxis_sub_temp do
-    
+    association :praxis_result_temp
+    sub_number {1}
+    name {Book.title}
+    pts_earned {Number.between(1, 20)}
+    pts_aval {20}
+    avg_high {Number.between(10, 20)}
+    avg_low {Number.between(1, 10)}
   end
 end
--- test/models/praxis_result_temp_test.rb
+++ test/models/praxis_result_temp_test.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: praxis_result_temps
+#
+#  id             :integer          not null, primary key
+#  first_name     :string(255)
+#  last_name      :string(255)
+#  student_id     :integer
+#  praxis_test_id :integer
+#  test_date      :datetime
+#  test_score     :integer
+#  best_score     :integer
+#
+
 require 'test_helper'
 
 class PraxisResultTempTest < ActiveSupport::TestCase
