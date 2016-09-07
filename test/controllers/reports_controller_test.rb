e06cd2749396f9fead6762d0be1e49b7546f38de
Jacob Stoebel <stoebelj@berea.edu>
2016-09-07 14:16:26 -0400

    added broken tests for controller

--- test/controllers/reports_controller_test.rb
+++ test/controllers/reports_controller_test.rb
@@ -1,14 +1,50 @@
 require 'test_helper'
 
 class ReportsControllerTest < ActionController::TestCase
-  test "should get index" do
-    get :index
-    assert_response :success
-  end
 
-  test "should get show" do
-    get :show
-    assert_response :success
+  describe "index" do
+
+    before do
+
+      @records = []
+
+      ["227", "228", "440", "479"].each do |code|
+        course = FactoryGirl.create :transcript, {:course_code => "EDS#{code}",
+          :grade_pt => 3.0
+        }
+        @records.push course
+      end
+      get :index
+
+    end
+
+    test "http 200" do
+      assert false
+    end
+
+    describe "pulls records" do
+
+      test "basic info matches" do
+        # Bnum though Minors is correct for each record
+        assert false
+      end
+
+      test "matches taken 227/228" do
+        assert false
+
+      end
+
+      test "matches completed 227/228" do
+        assert false
+
+      end
+
+      test "matches term for 440/479" do
+        assert false
+      end
+
+    end
+
   end
 
 end
