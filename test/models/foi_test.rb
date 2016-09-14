d4b90f59866440774f0c9f985a2abb163b840434
Jacob Stoebel <stoebelj@berea.edu>
2016-09-14 17:33:18 -0400

    additional tests for foi_model
    test _import_foi
    test import

--- test/models/foi_test.rb
+++ test/models/foi_test.rb
@@ -16,54 +16,130 @@
 require 'test_helper'
 
 class FoiTest < ActiveSupport::TestCase
-  
+
   describe "basic validations" do
-  
-    foi = Foi.new
-    foi.valid?
-    foi.attributes.except("id", "created_at", "updated_at").keys.each do |key|
-      test "missing #{key}" do
-        assert_equal @foi.errors[key.to_sym], ["can't be blank"] 
-      end
+
+    before do
+      @foi = Foi.new
+      @foi.valid?
     end
-  
+
+    test "student_id" do
+      assert_equal ["Student could not be identified."], @foi.errors[:student_id]
+    end
+
+    test "date_completing" do
+      assert_equal ["Date completing is missing or incorrectly formatted. Example format: 01/01/2016 09:00:00 AM"], @foi.errors[:date_completing]
+    end
+
+    test "new_form" do
+      assert_equal ["Can't determine if this is a new form."], @foi.errors[:new_form]
+    end
+
+    test "major_id" do
+      assert_equal ["Major could not be determined."], @foi.errors[:major_id]
+    end
+
+    test "seek_cert" do
+      assert_equal ["Could not determine if student is seeking certification."], @foi.errors[:seek_cert]
+    end
+
+    test "eds_only" do
+      assert_equal ["Could not determine if student is seeking EDS only."], @foi.errors[:eds_only]
+    end
+
   end
-  
+
   describe "_import_foi" do
-    
     before do
       @stu = FactoryGirl.create :student
-      
-      @row = {"Q1.2_3 - B#" => @stu.Bnum , 
-      "Recorded Date" => Date.today.strftime("%m/%d/%Y %I:%M:%S %p"),
-      "Q1.3 - Are you completing this form for the first time, or is this form a revision..." => "yes",
-      "Q3.1 - Which area do you wish to seek certification in?" => Major.first.name,
-      "Q1.4 - Do you intend to seek teacher certification at Berea College?" => "yes", 
-      "Q2.1 - Do you intend to seek an Education Studies degree without certification?" => "yes"
+      @row = {"Q1.2_3 - B#" => @stu.Bnum ,
+        "Recorded Date" => Date.today.strftime("%m/%d/%Y %I:%M:%S %p"),
+        "Q1.3 - Are you completing this form for the first time, or is this form a revision..." => "new form",
+        "Q3.1 - Which area do you wish to seek certification in?" => Major.first.name,
+        "Q1.4 - Do you intend to seek teacher certification at Berea College?" => "yes",
+        "Q2.1 - Do you intend to seek an Education Studies degree without certification?" => "yes"
       }
     end
-    
-    test "imports row" do
-      assert_difference('Foi.count', 1) do
-        Foi._import_foi(@row)
+
+    describe "successful import" do
+
+      test "imports row" do
+        assert_difference('Foi.count', 1) do
+          Foi._import_foi(@row)
+        end
       end
-    
+
+      test "foi matches student" do
+        Foi._import_foi(@row)
+        assert_equal 1, @stu.foi.size
+      end
     end
-    
-    # describe "doesn't import row - missing param" do
-      
-    #   must_have = ["Q1.2_3 - B#", "Recorded Date", "Q3.1 - Which area do you wish to seek certification in?",   ]
-    #   must_have.each do |k|
-      
-    #     test "missing k" do
-    #         @row[k] = nil
-            
-    #     end
-      
-    #   end
-      
-    # end
-    
+
+    describe "doesn't import row - missing param" do
+
+      must_have = ["Q1.2_3 - B#",
+        "Recorded Date",
+        "Q3.1 - Which area do you wish to seek certification in?",
+        "Q1.4 - Do you intend to seek teacher certification at Berea College?",
+        "Q2.1 - Do you intend to seek an Education Studies degree without certification?"
+         ]
+
+      must_have.each do |k|
+
+        test "missing #{k}" do
+            @row[k] = nil
+
+            # should throw an error
+            assert_raises ActiveRecord::RecordInvalid do
+              Foi._import_foi(@row)
+            end
+          end
+
+      end # loop
+
+    end #inner describe
+
+  end # outer describe
+
+  describe "import" do
+
+    describe "successful record" do
+
+      before do
+        # create the CSV file
+        @stu = FactoryGirl.create :student
+        @expected_attrs = {"Q1.2_3 - B#" => @stu.Bnum,
+          "Recorded Date" => Date.today.strftime("%m/%d/%Y %I:%M:%S %p"),
+          "Q1.3 - Are you completing this form for the first time, or is this form a revision..." => "new form",
+          "Q3.1 - Which area do you wish to seek certification in?" => Major.first.name,
+          "Q1.4 - Do you intend to seek teacher certification at Berea College?" => "yes",
+          "Q2.1 - Do you intend to seek an Education Studies degree without certification?" => "yes"
+        }
+
+        headers = @expected_attrs.keys
+        @test_file_loc = Rails.root.join('test', 'test_temp', 'test_foi.csv')
+
+        CSV.open(@test_file_loc, "w") do |csv|
+          csv << @expected_attrs.keys
+          csv << @expected_attrs.values
+        end
+        puts @test_file_loc
+      end # before
+
+      test "creates a FOI" do
+        assert_difference("Foi.count", 1) do
+          Foi.import(@test_file_loc)
+        end
+      end
+
+      test "Foi matches student" do
+        Foi.import(@test_file_loc)
+        assert_equal 1, @stu.foi.size
+      end
+
+    end
+
   end
 
 end
