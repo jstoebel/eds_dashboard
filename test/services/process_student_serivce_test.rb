b7fdb33ff038a6d74277a96f976fde8a33499b83
Jacob Stoebel <stoebelj@berea.edu>
2016-09-22 11:30:39 -0400

    fixed bug that was prevnting concentration1 from being stored
    added tests to compare attrs going in with attrs coming out

--- test/services/process_student_serivce_test.rb
+++ test/services/process_student_serivce_test.rb
@@ -75,31 +75,100 @@ class ProcessStudentServiceTest < ActiveSupport::TestCase
 
   ###TESTS FOR UPSERT###
   describe "upsert_student" do
-    it "creates a new student" do
 
-      stu_count0 = Student.all.size
-      new_stu = FactoryGirl.attributes_for :student
+    before do
 
-      new_attrs = {
-        "SZVEDSD_ID" => new_stu[:Bnum],
-        "SZVEDSD_LAST_NAME" => new_stu[:LastName],
-        "SZVEDSD_FIRST_NAME" => new_stu[:FirstName],
+      @stu_count0 = Student.all.size
+      @new_stu = FactoryGirl.attributes_for :student
+
+      @new_attrs = {
+        "SZVEDSD_ID" => @new_stu[:Bnum],
+        "SZVEDSD_LAST_NAME" => @new_stu[:LastName],
+        "SZVEDSD_FIRST_NAME" => @new_stu[:FirstName],
         "SZVEDSD_ENROLL_STAT" => "Active Student",
+        "SZVEDSD_MIDDLE_NAME" => @new_stu[:MiddleName],
+        "SZVEDSD_CLASSIFICATION" => @new_stu[:Classification],
+        "SZVEDSD_WITHDRAWALS" => @new_stu[:withdraws],
+        "SZVEDSD_TERM_GRADUATED" => @new_stu[:term_graduated],
+        "SZVEDSD_GENDER" => @new_stu[:gender],
+        "SZVEDSD_SSN" => nil,
+        "SZVEDSD_DOB" => nil,
+        "SZVEDSD_RACE" => @new_stu[:race],
+        "SZVEDSD_HISPANIC" => @new_stu[:hispanic] ? "yes" : "no",
+        "SZVEDSD_STATE" => nil,
+        "SZVEDSD_COUNTY" => nil,
+        "SZVEDSD_CITIZENSHIP" => nil,
+        "SZVEDSD_COUNTRY_ORIGIN" => nil,
+        "SZVEDSD_ESL" => nil,
+        "SZVEDSD_COMPUTER_TOEFL" => nil,
+        "SZVEDSD_TOEFL" => nil,
+        "SZVEDSD_TOEFL_ESSAY" => nil,
+        "SZVEDSD_TOEFL_LISTENING" => nil,
+        "SZVEDSD_TOEFL_READING" => nil,
+        "SZVEDSD_TOEFL_SPEAKING" => nil,
+        "SZVEDSD_TOEFL_STRUCT_WRITING" => nil,
+        "SZVEDSD_TOEFL_TOTAL" => nil,
+        "SZVEDSD_TOEFL_WRITING" => nil,
+        "SZVEDSD_TEST_WRITTEN_ENGLISH" => nil,
+        "SZVEDSD_IELTS_LISTENING" => nil,
+        "SZVEDSD_IELTS_READING" => nil,
+        "SZVEDSD_IELTS_WRITING" => nil,
+        "SZVEDSD_IELTS_SPEAKING" => nil,
+        "SZVEDSD_IELTS_BAND_SCORE" => nil,
+        "SZVEDSD_TRANSFER_STUDENT" => nil,
+        "SZVEDSD_TERM_EDS_EXPLOR_MJR" => nil,
+        "SZVEDSD_TERM_DECLARED_EDS_MJR" => nil,
+        "SZVEDSD_PRIMARY_MAJOR" => @new_stu[:CurrentMajor1],
+        "SZVEDSD_PRIMARY_MAJOR_CONC" => @new_stu[:concentration1],
+        "SZVEDSD_SECONDARY_MAJOR" => @new_stu[:CurrentMajor2],
+        "SZVEDSD_SECONDARY_MAJOR_CONC" => @new_stu[:concentration2],
+        "SZVEDSD_ADVISOR" => nil,
+        "SZVEDSD_HIGH_SCHOOL_GPA UMBER" => nil,
+        "SZVEDSD_HIGH_SCHOLL_RANK UMBER" => nil,
+        "SZVEDSD_ACT_ENGLISH" => nil,
+        "SZVEDSD_ACT_MATH" => nil,
+        "SZVEDSD_ACT_READING" => nil,
+        "SZVEDSD_ACT_SCIENCE_REASONING" => nil,
+        "SZVEDSD_ACT_COMPOSITE" => nil,
+        "SZVEDSD_SAT_VERB_CRIT_READING" => nil,
+        "SZVEDSD_SAT_MATH" => nil,
+        "SZVEDSD_SAT_WRITING" => nil,
+        "SZVEDSD_TERM_TAKEN" => nil,
+        "SZVEDSD_CRN" => nil,
+        "SZVEDSD_COURSE" => nil,
+        "SZVEDSD_GRADE" => nil,
+        "SZVEDSD_REGISTRATION_STAT" => nil,
+        "SZVEDSD_CREDITS_ATTEMPTED" => nil,
+        "SZVEDSD_CREDITS_EARNED" => nil,
+        "SZVEDSD_MINORS" => @new_stu[:CurrentMinors],
+        "SZVEDSD_GPA_IND" => nil,
+        "SZVEDSD_EMAIL" => nil,
+        "SZVEDSD_CPO" => @new_stu[:CPO],
+        "SZVEDSD_FILTER_TERM" => nil,
+        "SAVEDSD_INSTRUCTOR" =>nil
       }
 
-      row = @row_template.merge new_attrs
-      row_service = ProcessStudent.new row
-      row_service.upsert_student
-
-      expect Student.all.size == stu_count0 + 1
-
-      stu = Student.find_by :Bnum => new_stu[:Bnum]
-      filter_attrs = [:Bnum, :LastName, :FirstName, :EnrollmentStatus]
-
-      #created student and student found in DB should match across these params
-      assert_equal  new_attrs.slice(*filter_attrs), stu.attributes.slice(*filter_attrs)
-
+      @row = @row_template.merge @new_attrs
+      @row_service = ProcessStudent.new @row
+      @row_service.upsert_student
+      @stu = Student.find_by :Bnum => @new_stu[:Bnum]
     end
+    test "creates a new student" do
+      assert Student.count == @stu_count0 + 1
+    end
+
+    describe "attrs match" do
+      filter_attrs = [:Bnum, :LastName, :FirstName, :MiddleName,
+        :EnrollmentStatus, :Classification, :withdraws, :term_graduated,
+        :gender, :race, :hispanic, :CurrentMajor1, :concentration1,
+        :CurrentMajor2, :concentration2, :CurrentMinors, :CPO
+      ]
+      filter_attrs.each do |attr|
+        test "with attr #{attr}" do
+          assert_equal @new_stu[attr], @stu.send(attr)
+        end # test
+      end # loop
+    end # describe
 
     it "updates a student" do
 
