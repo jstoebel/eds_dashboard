1d18c736c16ffcfcfbcd20ce3934ac20bc80f509
Jacob Stoebel <stoebelj@berea.edu>
2016-09-28 11:50:58 -0400

    add rake task to drop and rebuild all entrances and exits
    does not drop adm_tep where admit is false

--- app/models/program.rb
+++ app/models/program.rb
@@ -13,6 +13,6 @@ class Program < ActiveRecord::Base
 	has_many :adm_tep, {:foreign_key => 'Program_ProgCode'}
 	has_many :students, {:foreign_key => 'Program_ProgCode', through: :adm_tep}
 	has_many :prog_exits, {:foreign_key => 'Program_ProgCode'}
-    has_many :majors
-    has_many :praxis_tests, :foreign_key => "Program_ProgCode"
+  has_many :majors
+  has_many :praxis_tests, :foreign_key => "Program_ProgCode"
 end
