d9e04621d0fa29fb3951a451821f5b07b4651767
Jacob Stoebel <stoebelj@berea.edu>
2016-10-05 13:44:27 -0400

    only load seeds if target table is empty
    this allows for initial deployment in prod but avoids trampling pre existing data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
 #seed.rb
 require 'active_record/fixtures'
+
 #load these records from fixtures
 constants = ["banner_terms", "exit_codes", "programs", "roles", "majors", "praxis_tests"]
 constants.each do |c|
-  ActiveRecord::FixtureSet::create_fixtures(Rails.root.join("test", "fixtures"), c)
+  print "#{c}:"
+  model = c.camelize.singularize.constantize
+  if model.count == 0
+    print " -> loading..."
+    ActiveRecord::FixtureSet::create_fixtures(Rails.root.join("test", "fixtures"), c)
+    puts "-> done!"
+  else
+    puts " -> data exists, skipping"
+  end
 end
