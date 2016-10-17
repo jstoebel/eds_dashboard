feeb42c6474b1eaeb6cddb91d75270407b9fb6fb
Jacob Stoebel <stoebelj@berea.edu>
2016-10-17 13:50:46 -0400

    config for rbenv

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,6 +9,17 @@ set :repo_url, 'git@bitbucket.org:stoebelj/eds_dashboard.git'
 set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
+
+set :rbenv_type, :user # or :system, depends on your rbenv setup
+set :rbenv_ruby, 'ruby 2.3.1p112'
+
+# in case you want to set ruby version from the file:
+# set :rbenv_ruby, File.read('.ruby-version').strip
+set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
+set :rbenv_map_bins, %w{rake gem bundle ruby rails}
+set :rbenv_roles, :all # default value
+
+
 # Default value for :scm is :git
 # set :scm, :git
 
