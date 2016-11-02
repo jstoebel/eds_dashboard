c8adbff4de3cb9d4dc92ce9e1131c5186df5443a
Jacob Stoebel <stoebelj@berea.edu>
2016-11-02 16:17:18 -0400

    start of rails_admin

---
+++ config/initializers/rails_admin.rb
@@ -0,0 +1,60 @@
+RailsAdmin.config do |config|
+
+  ### Popular gems integration
+
+  ## == Devise ==
+  # config.authenticate_with do
+  #   warden.authenticate! scope: :user
+  # end
+  # config.current_user_method(&:current_user)
+
+  ## == Cancan ==
+  # config.authorize_with :cancan
+
+  ## == Pundit ==
+  # config.authorize_with :pundit
+
+  ## == PaperTrail ==
+  # config.audit_with :paper_trail, 'User', 'PaperTrail::Version' # PaperTrail >= 3.0.0
+
+  ### More at https://github.com/sferik/rails_admin/wiki/Base-configuration
+
+  ## == Gravatar integration ==
+  ## To disable Gravatar integration in Navigation Bar set to false
+  # config.show_gravatar true
+
+  # config.authorize_with do
+  #   # puts session
+  #   puts "*"*50
+  #   user = User.find_by :UserName => session[:user]
+  #   puts user.is? "admin"
+  #   puts "*"*50
+  #
+  #   if !(user.is? "admin")
+  #     raise "access denied"
+  #   end
+  #
+  #   # redirect_to "/access_denied" # unless current_user.is? "admin"
+  # end
+  # config.current_user_method(&:current_user)
+
+  config.authorize_with :cancancan #TODO add cancancan to rails_admin config
+  config.current_user_method { current_user }
+
+  config.excluded_models = ["Access", "Report"]
+
+  config.actions do
+    dashboard                     # mandatory
+    index                         # mandatory
+    new
+    export
+    bulk_delete
+    show
+    edit
+    delete
+    show_in_app
+    ## With an audit adapter, you can add:
+    # history_index
+    # history_show
+  end
+end
