89acca8151920f81874dc30fe95a750bf96fea9d
Jacob Stoebel <stoebelj@berea.edu>
2016-12-08 16:33:03 -0500

    more work on clinical_sites

--- test/controllers/clinical_sites_controller_test.rb
+++ test/controllers/clinical_sites_controller_test.rb
@@ -105,11 +105,13 @@ class ClinicalSitesControllerTest < ActionController::TestCase
 
           post :create, {:clinical_site => @site.attributes}
 
-          exepcted_attrs = @site.attributes
-          actual_attrs = assigns(:site).attributes
-          [exepcted_attrs, actual_attrs].map { |i| i.delete("id")}
+          expected_attrs = @site.attributes
+          expected_attrs["phone"] = PhonyRails.normalize_number(expected_attrs["phone"], country_code: 'EN')
 
-          assert_equal exepcted_attrs, actual_attrs
+          actual_attrs = assigns(:site).attributes
+          [expected_attrs, actual_attrs].map { |i| i.delete("id")}
+
+          assert_equal expected_attrs, actual_attrs
           assert_redirected_to clinical_sites_path
           assert_equal flash[:notice], "Created #{assigns(:site).SiteName}."
         end
@@ -119,7 +121,6 @@ class ClinicalSitesControllerTest < ActionController::TestCase
           post :create, {:clinical_site => @site.attributes}
 
           assert :successs
-          assert_equal expected_attrs, actual_attrs
           assert_equal flash[:notice], "Error creating site."
           assert_template "new"
         end
@@ -142,47 +143,34 @@ class ClinicalSitesControllerTest < ActionController::TestCase
         end
 
         test "should not get -- bad id" do
-          assert_raise(ActiveRecord::RecordNotFound) {get :delete, {:clinical_site_id => "bad id"}
+          assert_raise(ActiveRecord::RecordNotFound) {get :delete, {:clinical_site_id => "bad id"}}
         end
       end
     end
   end
 
-  test "should destroy site and dependent teachers and assignments" do
-    expected_term = BannerTerm.current_term(exact: false, plan_b: :forward)
-    role_names.each do |r|
-      load_session(r)
-      expected_site = FactoryGirl.create :clinical_site
+  describe "destroy" do
+    allowed_roles.each do |r|
+      describe "as #{r}" do
+        before do
+          load_session(r)
+          @assignment = FactoryGirl.create :clinical_assignment
+          @site = @assignment.clinical_teacher.clinical_site
+        end
 
-      expected_teacher = FactoryGirl.create :clinical_teacher, {
-        :clinical_site_id => expected_site.id
-      }
+        test "should post" do
 
-      expected_assign = FactoryGirl.create :clinical_assignment, {
-        :clinical_teacher_id => expected_teacher.id,
-        :Term => expected_term.id,
-        :StartDate => expected_term.StartDate.strftime("%Y/%m/%d"),
-        :EndDate => expected_term.EndDate.strftime("%Y/%m/%d")
-      }
+          post :destroy, {:id => @site.id}
 
-      post :destroy, {:id => expected_site.id}
+          assert_equal(@site, assigns(:site))
+          assert assigns(:site).destroyed?
+          assigns(:site).clinical_teachers.each{|i| assert i.destroyed?}    #should auto delete any teacher assignments when teacher destroyed
+          assigns(:site).clinical_teachers.each{|i| i.clinical_assignments.each{|j| assert j.destroyed?}}    #are all assignments destroyed?
+          assert_equal flash[:notice], "Deleted Successfully"
+          assert_redirected_to(clinical_sites_path)
+        end
 
-      assert_equal(expected_site, assigns(:site))
-      assert assigns(:site).destroyed?
-      assigns(:site).clinical_teachers.each{|i| assert i.destroyed?}    #should auto delete any teacher assignments when teacher destroyed
-      assigns(:site).clinical_teachers.each{|i| i.clinical_assignments.each{|j| assert j.destroyed?}}    #are all assignments destroyed?
-      assert_equal flash[:notice], "Deleted Successfully"
-      assert_redirected_to(clinical_sites_path)
-    end
-  end
-
-  test "should not destroy site bad role" do
-
-    (role_names - allowed_roles).each do |r|
-      load_session(r)
-      expected_site = FactoryGirl.create :clinical_site
-      post :destroy, {:id => expected_site.id}
-      assert_redirected_to "/access_denied"
+      end
     end
   end
 
