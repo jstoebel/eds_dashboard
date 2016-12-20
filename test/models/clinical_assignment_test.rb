489acbebe9fbfb8d0c43ba0ef82f753c570f8597
Jacob Stoebel <stoebelj@berea.edu>
2016-12-20 15:30:16 -0500

    clinical_assignment_test

--- test/models/clinical_assignment_test.rb
+++ test/models/clinical_assignment_test.rb
@@ -17,14 +17,14 @@ require 'test_helper'
 class ClinicalAssignmentTest < ActiveSupport::TestCase
 
 	test "no teacher id" do
-		assignment = ClinicalAssignment.first
+		assignment = FactoryGirl.create :clinical_assignment
 		assignment.clinical_teacher_id = nil
 		assignment.valid?
-		py_assert(["Please select a clinical teacher."], assignment.errors[:clinical_teacher_id])	
+		py_assert(["Please select a clinical teacher."], assignment.errors[:clinical_teacher_id])
 	end
 
 	test "unique assignment catch" do
-		assignment = ClinicalAssignment.first
+		assignment = FactoryGirl.create :clinical_assignment
 		assignment2 = assignment.clone
 		assignment2.id = nil
 		assignment2.valid?
@@ -32,7 +32,7 @@ class ClinicalAssignmentTest < ActiveSupport::TestCase
 	end
 
 	test "unique assignment pass" do
-		assignment = ClinicalAssignment.first
+		assignment = FactoryGirl.create :clinical_assignment
 		assignment2 = ClinicalAssignment.new(assignment.attributes)
 		assignment2.CourseID = "EDS335"		#a differing course should allow this to pass
 		assignment2.valid?
@@ -41,7 +41,7 @@ class ClinicalAssignmentTest < ActiveSupport::TestCase
 	end
 
 	test "need bnum" do
-		assignment = ClinicalAssignment.first
+		assignment = FactoryGirl.create :clinical_assignment
 		assignment.student_id = nil
 		assignment.valid?
 		py_assert(["Please select a student."], assignment.errors[:student_id])
@@ -49,21 +49,21 @@ class ClinicalAssignmentTest < ActiveSupport::TestCase
 	end
 
 	test "need start date" do
-		assignment = ClinicalAssignment.first
+		assignment = FactoryGirl.create :clinical_assignment
 		assignment.StartDate = nil
 		assignment.valid?
 		py_assert(["Please enter a valid start date."], assignment.errors[:StartDate])
 	end
 
 	test "need end date" do
-		assignment = ClinicalAssignment.first
+		assignment = FactoryGirl.create :clinical_assignment
 		assignment.EndDate = nil
 		assignment.valid?
 		py_assert(["Please enter a valid end date."], assignment.errors[:EndDate])
 	end
 
 	test "start before end" do
-		assignment = ClinicalAssignment.first
+		assignment = FactoryGirl.create :clinical_assignment
 		#swap start and end dates
 		new_start = assignment.EndDate
 		new_end = assignment.StartDate
@@ -72,5 +72,5 @@ class ClinicalAssignmentTest < ActiveSupport::TestCase
 		assignment.valid?
 		py_assert(["Start date must be before end date."], assignment.errors[:base])
 	end
-		
+
 end
