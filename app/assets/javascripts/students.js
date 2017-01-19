d50e6450966be4a22a777d25992221d5d3e6217b
Jacob Stoebel <jstoebel@gmail.com>
2017-01-19 11:45:43 -0500

    load menu options on menu click
    this will hopefully speed up students#index page load a bit

--- app/assets/javascripts/students.js
+++ app/assets/javascripts/students.js
@@ -1,24 +1,43 @@
 var updateStatus = function(stuID, status, comment) {
   // attempts to update a student with the values of status and comment
 
-    return new Promise(function(resolve, reject){
-        $.ajax({
-            type: "PATCH",
-            dataType: "json",
-            url: "/students/" + stuID + "/update_presumed_status",
-            data: {authenticity_token : AUTH_TOKEN, student: {presumed_status: status, presumed_status_comment: comment } },
-            success: function(data){
-                resolve(data);
-            },
-            error: function(xhr, textStatus, errorThrown){
-              var json = $.parseJSON(xhr.responseText);
-              reject(json);
-            }
-        })
-    })
-
+  return new Promise(function(resolve, reject){
+      $.ajax({
+          type: "PATCH",
+          dataType: "json",
+          url: "/students/" + stuID + "/update_presumed_status",
+          data: {authenticity_token : AUTH_TOKEN, student: {presumed_status: status, presumed_status_comment: comment } },
+          success: function(data){
+              resolve(data);
+          },
+          error: function(xhr, textStatus, errorThrown){
+            var json = $.parseJSON(xhr.responseText);
+            reject(json);
+          }
+      })
+  })
 }
 
+
+var getResources = function(stuID) {
+  // gets resources available for a student
+
+  return new Promise(function(resolve, reject){
+      $.ajax({
+          type: "GET",
+          dataType: "json",
+          url: "/students/" + stuID + "/get_resources",
+          data: {},
+          success: function(data){
+              resolve(data);
+          },
+          error: function(xhr, textStatus, errorThrown){
+            var json = $.parseJSON(xhr.responseText);
+            reject(json);
+          }
+      })
+  })
+}
 
 $(document).ready(function(){
 
@@ -48,6 +67,31 @@ $(document).ready(function(){
     })
   }) // change presumed status
 
-  
+
+
+  // var actionsMenus = $(".student-actions-menu")
+  $(".student-actions-menu").on("click", function(event){
+
+    var dropDown = $(this);
+    var stuID = event.target.id.match(/stu-actions-menu-(\d+)/)[1]
+
+    var actionsMenu = dropDown.find('ul:first')
+    actionsMenu.empty();
+    getResources(stuID).then(function(response){
+
+      // populate the menu
+      response.forEach(function(row){
+        if (!row.disable){
+          // row should be presented
+          var menuItem = "<li><a href='"+ row.link_url + "'> " + row.menu_name + " </a></li>"
+          actionsMenu.append(menuItem)
+        }
+      })
+
+    }).catch(function(json){
+      console.log(json);
+    })
+
+  })
 
 })
