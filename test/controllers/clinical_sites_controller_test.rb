a4086fa2ac9889bb7bc33c01676b57a27866c63d
wisemanh <wisemanh@berea.edu>
2017-02-16 17:41:54 +0000

    Added clinical sites show view, route, updated button and index, and tests

--- test/controllers/clinical_sites_controller_test.rb
+++ test/controllers/clinical_sites_controller_test.rb
@@ -20,7 +20,7 @@ class ClinicalSitesControllerTest < ActionController::TestCase
 
   #all roles have access to this resource
   allowed_roles = ["admin", "advisor", "staff", "student labor"]
-
+  all_roles = Role.all.pluck :RoleName
   describe "index" do
     allowed_roles.each do |r|
       before do
@@ -58,7 +58,37 @@ class ClinicalSitesControllerTest < ActionController::TestCase
       end
     end
   end
-
+  
+  describe "show" do
+    allowed_roles.each do |r|
+      describe "as #{r}" do
+        before do
+          load_session(r)
+          @site = FactoryGirl.create :clinical_site
+        end
+        
+        test "should get show" do
+          get :show, :id => @site.id
+          assert_response :success
+        end
+        
+      end
+    end
+    (all_roles - allowed_roles).each do |r|
+      describe "as #{r}" do
+        before do 
+          load_session(r)
+          @site = FactoryGirl.create :clinical_site
+        end
+        
+        test "Should not show" do
+          get :show, :id => @site.id
+          assert_redirected_to "/access_denied"
+        end
+      end
+    end
+  end
+  
   describe "update" do
     allowed_roles.each do |r|
       describe "as #{r}" do
