c1fc940f73f57b2e7850f034efa0cdc5fb5f87e7
Jacob Stoebel <jstoebel@gmail.com>
2017-03-13 13:03:52 -0400

    add FG attr for item_level: passing

--- test/factories/item_levels.rb
+++ test/factories/item_levels.rb
@@ -20,6 +20,7 @@ FactoryGirl.define do
     descriptor Lorem.paragraph
     level Lorem.word
     sequence(:ord, (1..4).cycle)
+    passing {Boolean.boolean 0.5}
 
     factory :level_with_scores do
 
