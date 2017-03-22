11e9ee84919b4f3ada61863b917552cf6a55cb62
Jacob Stoebel <jstoebel@gmail.com>
2017-03-22 12:22:24 -0400

    more work on import_moodle

--- app/controllers/student_scores_controller.rb
+++ app/controllers/student_scores_controller.rb
@@ -13,11 +13,18 @@ class StudentScoresController < ApplicationController
     file = params[:file]
     file_format = params[:format]
 
-    byebug
     # process the file. Should return an array containing results of
     # processing each record
 
+    # TODO: need the ability to distinguish between different assessments
+    begin
+      StudentScore.import_setup params[:file], params[:format]
+    rescue => e
+      flash[:notice] = e.message
+      # catch problems with the file here
+    end
 
+    redirect_to student_scores_path
   end
 
 end
--- app/models/student_score.rb
+++ app/models/student_score.rb
@@ -18,6 +18,12 @@ class StudentScore < ApplicationRecord
 
     validates_presence_of :student_id, :item_level_id, :scored_at
 
+    def self.import_setup(file, format)
+      # dispatch file to right method here
+      self.send("import_#{format}", file)
+
+    end
+
     def self.import_moodle(file)
       # file: a spreadsheet of records from moodle
       # post:
@@ -25,12 +31,57 @@ class StudentScore < ApplicationRecord
         # returns array of results for each record
 
       spreadsheet = Roo::Spreadsheet.open(file)
-      sheet = spreadsheet.sheet(0)
+      sheet = spreadsheet.sheet(0)  # assume the data is in the first sheet
+      # lets not assume that we know where the headers row is
 
-      
-      # assume the data is in the first sheet
+      headers_i = find_headers_index(sheet, "First name")
+      headers = sheet.row( headers_i )
 
 
+      self.transaction do
 
+        (headers_i + 1..sheet.count).each do |row_i|
+
+          begin
+            stu = Student.find_by! :Bnum => sheet.cell('c', row_i)
+          rescue
+            name = ('a'..'b').map{ |ltr| sheet.cell(ltr, row_i) }.join(' ')
+            raise "Could not find student at row #{row_i}: #{name}"
+          end
+
+          # index of each item_level definition
+          item_levels_indecies = headers.each_index.select{|i| headers[i] == 'Definition'}
+          item_levels_indecies.each do |level_i|
+            begin
+              level = ItemLevel.find_by! :descriptor =>  sheet.cell(row_i, level_i + 1)
+              StudentScore.create!({:student_id => stu.id, :item_level_id => level.id})
+
+            rescue
+              byebug
+              raise "Could not find descriptor at cell #{(65+level_i).chr}, #{row_i}: #{sheet.cell(row_i, level_i + 1)}"
+            end # error handling
+
+          end # item levels loop
+
+        end # records loop
+
+      end # transaction
+
+    end # import_moodle
+
+    private
+    def self.find_headers_index(sheet, evidence)
+      # finds the headers row of a sheet and returns the index
+      # defined as the row where the first cell is "First name"
+      # sheet: parsed roo sheet
+      # evidence (string): the value in first cell of a row that defines the header
+
+      sheet.each_with_index do |row, index|
+        if row[0] == evidence
+          return index + 1 # roo starts rows counting at 1 hence the add
+        end
+      end
+
+      raise "headers row not found" # catch me!
     end
 end
