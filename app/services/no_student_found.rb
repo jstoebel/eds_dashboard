40f27e9373359175a2a5c2f2cef5adb9e107f205
Jacob Stoebel <jstoebel@gmail.com>
2017-06-21 15:07:04 -0400

    remove unused files
    including chef config
    various sample data
    other random stuff we no longer need

--- app/services/no_student_found.rb
+++
@@ -1,2 +0,0 @@
-class NoStudentFound < Exception
-end
--- config/chef/site-cookbooks/eds_dashboard/attributes/default.rb
+++
@@ -1,6 +0,0 @@
-default['app']               = 'eds_dashboard'
-
-default['nodejs']['dir']     = '/usr/local'
-default['nodejs']['version'] = '4.6.0'
-
-default['ruby']['version']   = '2.3.1'
