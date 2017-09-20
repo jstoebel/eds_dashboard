2018665eec0de863085da802833c21d3b159eefc
Jacob Stoebel <jstoebel@gmail.com>
2017-09-20 16:00:16 -0400

    smtp error handling for issue_update#create
    this controller action did not have an exception handing in place when an smtp error was encountered (when an alert email goes out)
    I added such handing and also added tests to issue and issue_update stubbing IssueUpdate#alert_created to raise an smtp exception

--- app/controllers/issue_updates_controller.rb
+++ app/controllers/issue_updates_controller.rb
@@ -13,13 +13,12 @@
 #  addressed                :boolean
 #  status                   :string(255)
 #
-
 class IssueUpdatesController < ApplicationController
   authorize_resource
   layout 'application'
 
   def new
-    #sets up creation of a new issue update
+    # sets up creation of a new issue update
     @issue = Issue.find(params[:issue_id])
     authorize! :manage, @issue
 
@@ -29,17 +28,17 @@ class IssueUpdatesController < ApplicationController
     @update = IssueUpdate.new
 
     name_details(@student)
-
   end
 
   def create
-    #saves a new update to the database, redirects to index if successful, rerenders page if error.
+    # saves a new update to the database, redirects to index
+    # if successful, rerenders page if error.
     @issue = Issue.find(params[:issue_id])
 
     @update = IssueUpdate.new(issue_update_params)
     @update.Issues_IssueID = @issue.IssueID
 
-    #assign advisor's B#
+    # assign advisor's B#
     user = current_user
 
     @update.tep_advisors_AdvisorBnum = user.tep_advisor.andand.id
@@ -49,37 +48,47 @@ class IssueUpdatesController < ApplicationController
     @student = Student.find(@issue.student.id)
     authorize! :read, @student
 
-    if @update.save
-      flash[:info] = "New update added"
-      redirect_to(issue_issue_updates_path(@issue.IssueID))
-    else
-      render('new')
-    end
+    begin
+      if @update.save
+        flash[:info] = 'New update added'
+        redirect_to(issue_issue_updates_path(@issue.IssueID))
+      else
+        render('new')
+      end
+    rescue Net::SMTPAuthenticationError => e
+      # email failed to send
+      Rails.logger.warn e.message
+      Rails.logger.warn e.backtrace
 
-  end
+      flash[:info] = 'New update added but there was a problem sending email alerts. ' \
+                     'If this problem persists, please alert the administrator.'
+      redirect_to(issue_issue_updates_path(@issue.IssueID))
+    end # rescue
+  end # create
 
   def index
-    #indexes updates for parent issue
+    # indexes updates for parent issue
     @issue = Issue.find(params[:issue_id])
     authorize! :read, @issue
     @student = Student.find(@issue.student.id)
     authorize! :read, @student
     name_details(@student)
-    @updates = @issue.issue_updates.sorted.where(:visible => true).select {|r| can? :read, r }  #no additional auth needed. If you can? the issue you can? the updates
+    @updates = @issue
+               .issue_updates
+               .sorted.where(visible: true)
+               .select { |r| can? :read, r } # no additional auth needed. If you can? the issue you can? the updates
   end
 
   def destroy
-    #should destory records and make them not visible to the user,
+    # should destory records and make them not visible to the user,
     # but still presist in the database
-    
     authorize! :read, @manage
     @update = IssueUpdate.find(params[:id])
     @update.visible = false
     @update.save
-    flash[:info] = "Deleted Successfully!"
+    flash[:info] = 'Deleted Successfully!'
     redirect_to(issue_issue_updates_path(@update.issue.id))
   end
-
 
   def update
     # user may toggle if the issue is addressed attr
@@ -87,19 +96,17 @@ class IssueUpdatesController < ApplicationController
     authorize! :manage, update
     update.addressed = params[:issue_update][:addressed]
 
-    response = {:json => update}
     if update.save
-      render :json => update, status: :created
+      render json: update, status: :created
     else
-      render :json => update, status: :unprocessable_entity
+      render json: update, status: :unprocessable_entity
     end
-
   end
 
   private
+
   def close_issue_params
     params.require(:issue_update).permit(:Description)
-
   end
 
   def issue_update_params
@@ -107,5 +114,4 @@ class IssueUpdatesController < ApplicationController
       .require(:issue_updates)
       .permit(:UpdateName, :Description, :status, :addressed)
   end
-
 end
