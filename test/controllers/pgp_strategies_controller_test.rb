77bf5dead3eb025195d6c26fb0d8980547d15c4f
Jacob Stoebel <jstoebel@gmail.com>
2017-12-12 16:32:05 -0500

    pgp_strategies controller was not implemented

--- test/controllers/pgp_strategies_controller_test.rb
+++ test/controllers/pgp_strategies_controller_test.rb
@@ -2,41 +2,171 @@ require 'test_helper'
 
 class PgpStrategiesControllerTest < ActionDispatch::IntegrationTest
 
-  describe 'index' do
-    
-    before do
-      stu = FactoryGirl.create :student
-      strats = FactoryGirl.create_list :pgp_strategy, 3, student: stu
+  before do
+    @pgp_goal = FactoryGirl.create :pgp_goal
+    @strategies = FactoryGirl.create_list :pgp_strategy, 3, pgp_goal: @pgp_goal 
+  end
 
+  describe 'INDEX' do
+    let(:get_index) do
+      get pgp_goal_pgp_strategies_path @pgp_goal.id
+    end
+        
+    it 'pulls @pgp_strategies' do
 
-      get 
+      # deactivate a strategy
+      @strategies.first.update_attributes! active: false
+      get_index
+      assert_equal @pgp_strategies.order(:active).reverse_order.to_a,
+                   assigns(:pgp_strategies)
+    end
+
+    it 'returns 200 ok' do
+      assert_response :success
+    end
+
+    it 'renders index template' do
+      assert_template :index
     end
 
   end
 
-  test "should get index" do
-    get pgp_strategies_index_url
-    assert_response :success
+  describe 'NEW' do
+    before do
+      get new_pgp_strategy_path
+    end
+
+    it 'pulls fresh @pgp_strategy' do
+      assert assigns(:pgp_strategy).new_record?
+    end
+
+    it 'returns 200 ok' do
+      assert_response :success
+    end
+
+    it 'renders new template' do
+      assert_template :new
+    end
+
   end
 
-  test "should get new" do
-    get pgp_strategies_new_url
-    assert_response :success
+  describe 'CREATE' do
+
+    before do
+      @strategy = FactoryGirl.build :pgp_strategy
+      @goal = @strategy.pgp_goal
+    end
+
+    let(:post_create) do
+      post pgp_strategies_path,
+           params: { :pgp_goal_id => @goal.id, pgp_strategy: @strategy.attributes}
+    end
+
+    let(:stub_save) {PgpStrategy.any_instance.stubs(:save).returns(false)}
+
+    describe 'happy path' do
+
+      it 'creates a @pgp_strategy' do
+        assert_difference -> { PgpStrategy.count }, 1 do
+          post_create
+        end
+      end
+
+      it 'redirects' do
+        post_create
+        assert_redirected_to pgp_goal_pgp_strategies_path(@pgp_goal.id)
+      end
+    end
+
+    describe 'sad path' do
+      before do
+        stub_save
+      end
+      it 'creates a @pgp_strategy' do
+        assert_difference -> { PgpStrategy.count }, 0 do
+          post_create
+        end
+      end
+
+      it 'redirects' do
+        post_create
+        assert_redirected_to pgp_goal_pgp_strategies_path(@pgp_goal.id)
+      end
+
+    end
+
+  end # CREATE
+
+  describe 'EDIT' do
+    before do
+      get edit_pgp_goal_pgp_strategy_path @pgp_goal.id
+    end
+
+    it 'pulls @pgp_score' do
+      assert_equal @pgp_strategy, assigns(:pgp_strategy)
+    end
+
+    it 'pulls pgp_goal' do
+      assert_equal @pgp_goal, assigns(:pgp_goal)
+    end
+
+    it 'returns 200 ok' do
+      assert_response :success
+    end
+
+    it 'renders edit template' do
+      assert_template :edit
+    end
   end
 
-  test "should get create" do
-    get pgp_strategies_create_url
-    assert_response :success
-  end
+  describe 'UPDATE' do
 
-  test "should get edit" do
-    get pgp_strategies_edit_url
-    assert_response :success
-  end
+    before do
+      @strategy = FactoryGirl.build :pgp_strategy
+      @goal = @strategy.pgp_goal
+    end
 
-  test "should get update" do
-    get pgp_strategies_update_url
-    assert_response :success
-  end
+    let(:patch_update) do
+      @new_params = @strategy.attributes.merge(name: 'new name')
+      patch pgp_goal_pgp_strategy_path,
+            params: { :pgp_goal_id => @goal.id, id: @strategy.id, pgp_strategy: @new_params}
+    end
+
+    let(:stub_save) {PgpStrategy.any_instance.stubs(:save).returns(false)}
+
+    describe 'happy path' do
+
+      it 'updates params' do
+        patch_update
+        @new_params.each do |key, val|
+          asser_equal val, assigns(:pgp_strategy).send(key)
+        end
+      end
+
+      it 'redirects' do
+        patch_update
+        assert_redirected_to pgp_goal_pgp_strategies_path(@pgp_goal.id)
+      end
+
+      it 'returns http 302' do
+        assert_response 302
+      end
+    end
+
+    describe 'sad path' do
+      before do
+        stub_save
+      end
+
+      it 'pulls @pgp_goal' do
+        assert_equal @pgp_goal, assigns(:pgp_goal)
+      end
+
+      it 'renders edit template' do
+        assert_template :edit
+      end
+
+    end
+  end # CREATE
 
 end
